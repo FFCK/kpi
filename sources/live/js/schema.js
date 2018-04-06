70f8f37991042799546a300c387048e41ddb887f
laurent.garrigue <lgarrigue@gmail.com>
2018-04-06 17:04:57 +0200

    TV Control panel - refresh schema & frame_terrains

--- sources/live/js/schema.js
+++ sources/live/js/schema.js
@@ -1,5 +1,13 @@
 function Init(voie)
 {
 	SetVoie(voie);
+    
+    // Refresh de la page toute les 60 secondes ...
+	window.setInterval("reFresh()",60000);
 }	
+
+
+function reFresh() {
+    location.reload(true);
+}
 
