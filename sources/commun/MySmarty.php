e2c5fdc17db5187effb3e2439d0b591995275fbd
laurent.garrigue <lgarrigue@gmail.com>
2018-04-09 09:30:28 +0200

    Fix Docker-compose MySmarty générique

--- sources/commun/MySmarty.php
+++ sources/commun/MySmarty.php
@@ -2,11 +2,7 @@
 
 // charge la librairie Smarty
 include_once('MyConfig.php');
-	  
-if (PRODUCTION)	 
-	require('/home/users2/p/poloweb/www/agil/Smarty-Lib/Smarty.class.php');
-else
-	require('/var/www/html/kpi/Smarty-Lib/Smarty.class.php');
+require(PATH_ABS.'Smarty-Lib/Smarty.class.php');
 			   
 $smarty = new Smarty();
 
@@ -19,24 +15,13 @@ class MySmarty extends Smarty
 		
 		$this->Smarty();
 							   
-		if (PRODUCTION)
-		{
-			$this->template_dir = '/home/users2/p/poloweb/www/agil/smarty/templates';
-			$this->compile_dir = '/home/users2/p/poloweb/www/agil/smarty/templates_c';
-			$this->cache_dir = '/home/users2/p/poloweb/www/agil/smarty/cache';
-			$this->config_dir = '/home/users2/p/poloweb/www/agil/smarty/configs';
-		}
-		else
-		{
-			$this->template_dir = '/var/www/html/kpi/smarty/templates';
-			$this->compile_dir = '/var/www/html/kpi/smarty/templates_c';
-			$this->cache_dir = '/var/www/html/kpi/smarty/cache';
-			$this->config_dir = '/var/www/html/kpi/smarty/configs';
-		}
+        $this->template_dir = PATH_ABS.'smarty/templates';
+        $this->compile_dir = PATH_ABS.'smarty/templates_c';
+        $this->cache_dir = PATH_ABS.'smarty/cache';
+        $this->config_dir = PATH_ABS.'smarty/configs';
 				
 		$this->caching = false;
 		$this->assign('app_name', 'KAYAK POLO');
 	}
 
 }
-?>
