2cf465a03942add668cab896972f73158a59f97b
laurent.garrigue <lgarrigue@gmail.com>
2018-06-02 23:56:49 +0200

    Test cartographie Leaflet + OSM

---
+++ sources/js/osm/test-osm.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+    Created on : 7 mai 2018
+    Author     : laurent
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Test OSM</title>
+        <!-- style -->
+        <link rel="stylesheet" type="text/css" href="../leaflet/leaflet.css" />
+        <link rel="stylesheet" type="text/css" href="test-osm.css" />
+    </head>
+    <body>
+        <div id="map"></div>
+        
+        <!-- Javascript -->
+        <script type="text/javascript" src="../leaflet/leaflet.js"></script>
+        <script type="text/javascript" src="leafletembed.js"></script>
+    </body>
+</html>
