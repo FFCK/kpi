1e372b77c11907df49b37dace8465f7cc9d8238d
laurent.garrigue <lgarrigue@gmail.com>
2018-08-26 09:58:47 +0200

    Ajustements leafletjs (affichage infos club même sans localisation)

--- sources/js/kpclubs.js
+++ sources/js/kpclubs.js
@@ -17,7 +17,8 @@ jq(document).ready(function() {
     var thisStartLat = 46.85;
     var thisStartLon = 1.73;
     // var thisPlotsAPIUrl='APIurl.php?format=leaflet&bbox='+minll.lng+','+minll.lat+','+maxll.lng+','+maxll.lat;
-    var thisPlotsAPIUrl = 'json-clubs.php';
+//    var thisPlotsAPIUrl = 'json-clubs.php';
+    var thisPlotsAPIUrl = 'clubs.json';
     var geocodApiUrl = 'https://api-adresse.data.gouv.fr/search/';
 
     var blueIcon = L.icon({
@@ -98,27 +99,35 @@ jq(document).ready(function() {
                 plotlist=eval("(" + ajaxRequest.responseText + ")");
                 removeMarkers();
                 for (i=0; i<plotlist.length; i++) {
-                    var plotll = new L.LatLng(plotlist[i].lat,plotlist[i].lon, true);
-                    var plotmark = new L.Marker(
-                        plotll, 
-                        {icon: blueIcon}
-                    );
-                    plotmark.data=plotlist[i];
-                    var layer = map.addLayer(plotmark);
-                    var code = plotmark.data.Code;
-                    var titre = '<b class="text-center">'+plotmark.data.Libelle+'</b>';
-                    plotmark.bindPopup(titre);
-                    plotmark.bindTooltip(plotmark.data.Libelle);
-                    clubList[code]=plotmark;
-                    plotmark.on('click', function(){
-                        afficheClub(this.data.Code);
-                        map.setView(this.getLatLng(), 10);
-                    });
-                    plotlayers.push(plotmark);
+                    var club = plotlist[i];
+                    clubList[club.Code] = club;
+                    // si le club est localisé
+                    if(club.lat != '') {
+                        var plotll = new L.LatLng(plotlist[i].lat,plotlist[i].lon, true);
+                        var plotmark = new L.Marker(
+                            plotll, 
+                            {icon: blueIcon}
+                        );
+                        plotmark.data=plotlist[i];
+                        var layer = map.addLayer(plotmark);
+                        var code = plotmark.data.Code;
+                        var titre = '<b class="text-center">'+plotmark.data.Libelle+'</b>';
+                        plotmark.bindPopup(titre);
+                        plotmark.bindTooltip(plotmark.data.Libelle);
+                        clubList[code].marker=plotmark;
+                        plotmark.on('click', function(){
+                            afficheClub(this.data.Code);
+                            map.setView(this.getLatLng(), 8);
+                            jq( "#localise" ).show();
+                        });
+                        plotlayers.push(plotmark);
+                    }
                 }
-                // si un club est pré-chargé, action !
+                // si un club est pré-chargé et qu'il a des coordonnées, action !
                 if(jq('#clubId').val() != '') {
-                    jq( "#coord" ).click();
+                    jq( "#localise" ).show().click();
+                } else {
+                    jq( "#localise" ).hide();
                 }
             }
         }
@@ -132,7 +141,7 @@ jq(document).ready(function() {
     }
 
     function afficheClub(clubId) {
-        club = clubList[clubId].data;
+        var club = clubList[clubId];
         jq('#clubId').val(clubId);
         jq('#clubLibelle').html(club.Libelle);
         jq('#clubLogo').html('<img class="img2" src="img/KIP/logo/'+clubId+'-logo.png" height="120" alt="">');
@@ -141,14 +150,21 @@ jq(document).ready(function() {
         jq('#www').html('<a href="'+club.www+'" target="_blank">'+club.www+'</a>');
         jq('#email').html(club.email);
         jq('#postal').html(club.Postal);
+        jq('#listEquipes').empty();
         club.Equipes.forEach(function(e) {
             var equipe = e.split('|');
             jq('#listEquipes').append('<a class="btn btn-sm btn-default" href="kpequipes.php?Equipe='+equipe[0]+'">'+equipe[1]+'</a>');
         });
     }
 
-    jq( "#coord" ).click(function(){
-        clubList[jq('#clubId').val()].fire('click');
+    jq( "#localise" ).click(function(){
+        var club = jq('#clubId').val();
+        afficheClub(club);
+        jq( "#localise" ).show();
+        var marker = clubList[club].marker;
+        marker.openPopup();
+        map.setView(marker.getLatLng(), 8);
+
     });
 
     jq( "#rechercheClub" ).autocomplete({
@@ -157,7 +173,7 @@ jq(document).ready(function() {
         select: function( event, ui ) {
             event.preventDefault();
             jq( "#clubId" ).val(ui.item.idClub);
-            jq( "#coord" ).click();
+            jq( "#localise" ).show().click();
         }
     });
     
@@ -179,158 +195,24 @@ jq(document).ready(function() {
         minLength: 2,
         select: function( event, ui ) {
             event.preventDefault();
-            console.log(ui.item);
             var lat = ui.item.lat;
             var lon = ui.item.lon;
             var plotll = new L.LatLng(lat,lon, true);
-            var plotmark = new L.Marker(plotll, {icon: redIcon});
+            var plotmark = new L.Marker(plotll, {icon: redIcon, draggable: true});
+            plotmark.on('dragend', function(event){
+                var position = this.getLatLng();
+                this.setLatLng(new L.LatLng(position.lat, position.lng),{draggable:'true'});
+                map.panTo(new L.LatLng(position.lat, position.lng))
+                var titre = '<div class="text-center"><i>'+ui.item.label+'</i><br><span class="coords">'+position.lat+','+position.lng+'</span></div>';
+                this.bindPopup(titre).openPopup();
+            });
             var layer = map.addLayer(plotmark);
-            var titre = '<div class="text-center"><b>'+ui.item.label+'</b><br>'+ui.item.lat+','+ui.item.lon+'</div>';
+            var titre = '<div class="text-center"><b>'+ui.item.label+'</b><br><span class="coords">'+ui.item.lat+','+ui.item.lon+'</span></div>';
             plotmark.bindPopup(titre).openPopup();
-            map.setView([lat, lon], 9);
+            map.setView([lat, lon], 8);
         }
     });
     
-//    var searchstring = function() {
-//        var str = jq('#address').val();
-//        var res = str.replace(/ /g, "+"); // remplace les espaces par des +
-//        
-//        // lance la requête en ajax
-//        var adressedatagouv = jq.ajax({
-//            url: "https://api-adresse.data.gouv.fr/search/?q=" + res + "&limit=" + limit,
-//            dataType: "json",
-//            responseType: "json",
-//            error: function(xhr) {
-//                alert(xhr.statusText);
-//            }
-//        });
-//        
-//        jq.when(adressedatagouv).done(function() {
-//            
-//        });
-//    
-//    }
-
-    //    var geocoder;
-    //    var carte;
-    //    var markers;
-    //    // affiche infoWindow au clic sur un marker
-    //    function bindInfoWindow(marker, map, infoWindow, html) {
-    //        google.maps.event.addListener(marker, 'click', function() {
-    //            map.setZoom(7);
-    //            map.setCenter(marker.getPosition());
-    //            infoWindow.setContent(html);
-    //            infoWindow.open(map, marker);
-    //            j('#clubId').val(j('#infoWindowContent').attr('data-code'));
-    //            //j('#clubLibelle').text(j('#infoWindowContent').attr('data-html'));
-    //            infosActivation();
-    //        });
-    //    }
-    //    // géocodage
-    //    function codeAddress() {
-    //        var address = document.getElementById('address').value;
-    //        geocoder.geocode( { 'address': address}, function(results, status) {
-    //            if (status == google.maps.GeocoderStatus.OK) {
-    //                carte.setCenter(results[0].geometry.location);
-    //                var marker = new google.maps.Marker({
-    //                    map: carte,
-    //                    position: results[0].geometry.location,
-    //
-    //                });
-    //                carte.setZoom(9);
-    //                var infoWindow = new google.maps.InfoWindow();
-    //                infoWindow.setContent('<i>' + address + '</i>'
-    //                        + '<br><br>' 
-    //                        + results[0].formatted_address
-    //                        + '<br>'
-    //                        + results[0].geometry.location
-    //                        );
-    //                infoWindow.open(carte, marker);
-    //            } else {
-    //                alert('Geocode was not successful for the following reason: ' + status);
-    //            }
-    //        });
-    //    }
-    //    //Charge le script google maps API et lance la fonction initialiser()
-    //    function loadScript() {
-    //        var script = document.createElement("script");
-    //        script.src = "https://maps.googleapis.com/maps/api/js?callback=initialiser&key=AIzaSyCeGM8c4y5LVVWhB-Rj07cSF8HWkvFiPXo";
-    //        document.body.appendChild(script);
-    //    }
-    //    window.onload = loadScript;
-    ////    window.onload = markActivation;
-    //
-    //// AUTRES FONCTIONS
-    //    j(function(){
-    //        if(j('#clubId').val() != ''){
-    //            infosActivation();
-    //            //markActivation(); 
-    //        }
-    //    });
-    //    //Activation infoWindow sur événement extérieur
-    //    function markActivation(){
-    //        var mark = j('#clubId').val();
-    //        if(markers[mark]){
-    //            google.maps.event.trigger(markers[mark], 'click');
-    //        }else{
-    //			infosActivation();
-    //		}
-    //    }
-    //    
-    //
-    //    function infosActivation(){
-    //        var clubId = j('#clubId').val();
-    //        j.get(
-    //            'loadClub.php',
-    //            {
-    //                term : clubId
-    //            },
-    //            function(data){
-    //                j( '#clubLibelle' ).html(data[0].label);
-    //                j( '#postal' ).html(data[0].postal !== null ? data[0].postal : '');
-    //                j( "#www" ).html(data[0].www !== null ? '<a class="btn btn-sm btn-default" href="'+data[0].www+'" target="_blank">'+data[0].www+'</a>' : '');
-    //                j( "#email" ).html(data[0].email !== null ? '<a class="btn btn-sm btn-default" href="mailto:'+data[0].email+'">'+data[0].email+'</a>' : '');
-    //                j( "#comitedep" ).text(data[0].comitedep !== null ? data[0].comitedep : '');
-    //                j( "#comitereg" ).text(data[0].comitereg !== null ? data[0].comitereg : '');
-    //                if( j( "#comitereg" ).text() == 'INTERNATIONAL' ){
-    //                    j( "#comitereg" ).parent().hide();
-    //                    j( "#comitedep" ).prev().text('Pays:');
-    //                } else {
-    //                    j( "#comitereg" ).parent().show();
-    //                    j( "#comitedep" ).prev().text('CD:');
-    //                }
-    //                j('#listEquipes').text('');
-    //                j.each(data[0].equipes, function(key, val) {
-    //                    j('#listEquipes').append('<a class="btn btn-sm btn-default" href="kpequipes.php?Equipe='+val.Numero+'">'+val.Libelle+'</a>');
-    //                });
-    //                if(data[0].logo != ''){
-    //                    j('#clubLogo').html('<img class="img2" src="'+data[0].logo+'" alt="'+data[0].club+'" />');
-    //                }else{
-    //                    j('#clubLogo').text('');
-    //                }
-    //                j( "#rechercheClub" ).val('');
-    //                data[0].coord !== null ? j( "#coord" ).show() : j( "#coord" ).hide();
-    //            },
-    //            'json'
-    //        );
-    //            
-    //    }
-    //    
-    //    j( "#coord" ).click(function(){
-    //        markActivation();
-    //    });
-    //    
-    //    j( "#rechercheClub" ).autocomplete({
-    //        source: 'searchClubs.php',
-    //        minLength: 2,
-    //        select: function( event, ui ) {
-    //            event.preventDefault();
-    //            j( "#clubId" ).val(ui.item.idClub);
-    //            markActivation();
-    //        }
-    //    });
-    //
-    //
 
 
 
