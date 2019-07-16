4f268b0d52283151aee91290a86437cf4b2f2e7e
laurent.garrigue <lgarrigue@gmail.com>
2019-07-16 22:15:13 +0200

    Fix #24 : Tentative création utilisateur existant

--- sources/commun/MyConfig.php
+++ sources/commun/MyConfig.php
@@ -1,10 +1,9 @@
 <?php
 // Configuration Generale 
-define('NUM_VERSION','2.28.3');
+define('NUM_VERSION','2.28.4');
 
 // Décalage horaire -35 minutes pour affichage des prochains matchs + match courant (kpmatchs.php)
 define('DECALAGE_MINUTES', '-395 minutes');
-
 
 
 require_once('MyParams.php');
