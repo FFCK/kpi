1f1c9b9fc4e230ecbc474e43bbe28f621afbe20e
laurent.garrigue <lgarrigue@gmail.com>
2020-04-22 11:36:20 +0200

    Corrections cron

--- docker/MyConfig.php
+++ docker/MyConfig.php
@@ -1,6 +1,9 @@
 <?php
 // Configuration Generale 
-define('NUM_VERSION','2.17.0');
+define('NUM_VERSION','2.18.0');
+
+// Décalage horaire -35 minutes pour affichage des prochains matchs + match courant (kpmatchs.php)
+define('DECALAGE_MINUTES', '-395 minutes');
 
 require_once('MyParams.php');
 
