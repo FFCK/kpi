398f9d21a447c8e23d073ec82b16afcc7bac5662
laurent.garrigue <lgarrigue@gmail.com>
2020-05-06 17:38:21 +0200

    KPTV boutons

--- sources/live/event.php
+++ sources/live/event.php
@@ -14,7 +14,7 @@ class Event extends MyPage
     {
     ?>
         <head>
-        <title>Event Live</title>
+        <title>Event cache generator</title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="author" content="F.F.C.K.">
