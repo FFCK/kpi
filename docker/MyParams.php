c9a18872acd2e5d5867a4894a06c8e00f583010d
laurentgarrigue <lgarrigue@gmail.com>
2020-06-21 23:23:56 +0200

    Params Docker

--- docker/MyParams.php
+++ docker/MyParams.php
@@ -4,20 +4,19 @@
 // Prod
 define('PARAM_PROD_LOGIN', 'root');
 define('PARAM_PROD_PASSWORD', 'root');
-// define('PARAM_PROD_DB', 'poloweb_03_09_2019');
-define('PARAM_PROD_DB', 'poloweb_10_10_2020');
+define('PARAM_PROD_DB', 'my_database');
 define('PARAM_PROD_SERVER', 'db');
 
 // Mirror
 define('PARAM_MIRROR_LOGIN', 'my_admin');
 define('PARAM_MIRROR_PASSWORD', 'my_passwd');
-define('PARAM_MIRROR_DB', 'poloweb_25_07_2019');
+define('PARAM_MIRROR_DB', 'my_database');
 define('PARAM_MIRROR_SERVER', 'db');
 
 // Local
 define('PARAM_LOCAL_LOGIN', 'my_admin');
 define('PARAM_LOCAL_PASSWORD', 'my_passwd');
-define('PARAM_LOCAL_DB', 'poloweb_25_07_2019');
+define('PARAM_LOCAL_DB', 'my_database');
 define('PARAM_LOCAL_SERVER', 'db');
 
 
@@ -32,6 +31,3 @@ define('PATH_ABS', '/var/www/html/');		// Chemin Absolu
 define('PATH_REL', './');									// Chemin Relatif 	
 define('MAIN_DIRECTORY', '');
 
-// Destinataires alertes cartons
-// define('CARD_ALERT_RECIPIENTS', 'contact@kayak-polo.info,frederic.escaffre@mpsa.com,eric.vignet@sidel.com,dejonghebrice@yahoo.fr,virginiebrackez@aol.com,denissaintemartine@gmail.com,c.moal@laposte.net');
-define('CARD_ALERT_RECIPIENTS', 'lgarrigue@gmail.com');
