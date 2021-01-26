064877fb4225ae57f5f533c65849a5509444b134
laurent.garrigue <lgarrigue@gmail.com>
2021-01-26 17:56:35 +0100

    Temp: requêtes joueurs et arbitres

--- SQL/Competiteurs_engages_competitions_nationales.sql
+++ SQL/Competiteurs_engages_competitions_nationales.sql
@@ -1,13 +1,16 @@
 /**
- * Compétiteurs FFCK engagés dans des compétitions nationales
+ * Liste compétiteurs FFCK engagés dans des compétitions nationales
  * sur une saison
  *
  * Author:  Laurent
  * Created: 30 oct. 2016
- * Updated: 13 dec. 2019
+ * Updated: 25 jan. 2021
  */
 
-SELECT gickp_Journees.Code_competition, gickp_Liste_Coureur.Matric, gickp_Liste_Coureur.Sexe, gickp_Liste_Coureur.Naissance, gickp_Liste_Coureur.Numero_club,gickp_Liste_Coureur.Numero_comite_dept, gickp_Liste_Coureur.Numero_comite_reg, gickp_Matchs_Joueurs.Capitaine 
+SELECT gickp_Journees.Code_competition, gickp_Liste_Coureur.Matric, gickp_Liste_Coureur.Sexe, 
+      gickp_Liste_Coureur.Naissance, gickp_Liste_Coureur.Numero_club,
+      gickp_Liste_Coureur.Numero_comite_dept, gickp_Liste_Coureur.Numero_comite_reg, 
+      gickp_Matchs_Joueurs.Capitaine 
 FROM gickp_Journees, gickp_Matchs, gickp_Matchs_Joueurs, gickp_Liste_Coureur
 WHERE (
     gickp_Journees.Code_competition LIKE 'N%' 
@@ -18,19 +21,19 @@ WHERE (
       )
 AND gickp_Liste_Coureur.Matric < 2000000
 -- AND gickp_Liste_Coureur.Sexe = "F"
-AND gickp_Journees.Code_saison = 2019
+AND gickp_Journees.Code_saison = 2020
 AND gickp_Matchs.Id_journee = gickp_Journees.Id
 AND gickp_Matchs_Joueurs.Id_match = gickp_Matchs.Id
 AND gickp_Liste_Coureur.Matric = gickp_Matchs_Joueurs.Matric
 GROUP BY gickp_Liste_Coureur.Matric
 
 /**
- * Compétiteurs FFCK engagés dans des compétitions nationales
+ * Nombre compétiteurs FFCK engagés dans des compétitions nationales
  * par club, sur une saison
  *
  * Author:  Laurent
  * Created: 8 jan. 2020
- * Updated: 16 feb. 2020
+ * Updated: 25 jan. 2021
  */
 SELECT LEFT(gickp_Liste_Coureur.Numero_comite_dept, 2) CD, gickp_Liste_Coureur.Numero_club Club, gickp_Club.Libelle, 
 COUNT(DISTINCT gickp_Liste_Coureur.Matric) Nb_joueurs
@@ -44,7 +47,7 @@ WHERE (
       )
 AND gickp_Liste_Coureur.Matric < 2000000
 AND gickp_Liste_Coureur.Numero_club = gickp_Club.Code
-AND gickp_Journees.Code_saison = 2019
+AND gickp_Journees.Code_saison = 2020
 AND gickp_Matchs.Id_journee = gickp_Journees.Id
 AND gickp_Matchs_Joueurs.Id_match = gickp_Matchs.Id
 AND gickp_Liste_Coureur.Matric = gickp_Matchs_Joueurs.Matric
@@ -53,12 +56,12 @@ GROUP BY gickp_Liste_Coureur.Numero_comite_dept, gickp_Liste_Coureur.Numero_club
 ORDER BY gickp_Liste_Coureur.Numero_comite_dept, gickp_Liste_Coureur.Numero_club
 
 /**
- * Compétiteurs FFCK engagés dans des compétitions nationales
+ * Nombre compétiteurs FFCK engagés dans des compétitions nationales
  * par département, sur une saison
  *
  * Author:  Laurent
  * Created: 16 feb. 2020
- * Updated: 16 feb. 2020
+ * Updated: 25 jan. 2021
  */
 SELECT LEFT(gickp_Liste_Coureur.Numero_comite_dept, 2) CD, 
 COUNT(DISTINCT gickp_Liste_Coureur.Matric) Nb_joueurs
@@ -72,7 +75,7 @@ WHERE (
       )
 AND gickp_Liste_Coureur.Matric < 2000000
 AND gickp_Liste_Coureur.Numero_club = gickp_Club.Code
-AND gickp_Journees.Code_saison = 2019
+AND gickp_Journees.Code_saison = 2020
 AND gickp_Matchs.Id_journee = gickp_Journees.Id
 AND gickp_Matchs_Joueurs.Id_match = gickp_Matchs.Id
 AND gickp_Liste_Coureur.Matric = gickp_Matchs_Joueurs.Matric
--- SQL/activite_arbitres.sql
+++ SQL/activite_arbitres.sql
@@ -23,3 +23,42 @@ AND j.Code_competition NOT LIKE 'M%'
 AND a.saison = 2018
 
 ORDER BY Nom, Prenom, Matric, Saison, Competition
+
+
+/**
+ * Liste arbitres ayant officié sur une compétition nationale 
+ * (arbitre principal ou secondaire)
+ *
+ * !!!!! REQUETE INCORRECTE !!!!!
+ *
+ * Author:  Laurent
+ * Created: 25 jan. 2021
+ * Updated: 25 jan. 2021
+ */
+ SELECT lc.Matric, lc.Nom, lc.Prenom, lc.Sexe, lc.Naissance, lc.Numero_club Club,
+	a.Arb, a.niveau, a.saison annee, 
+	SUM(IF(a.Matric = m.Matric_arbitre_principal,1,0)) Principal, 
+    SUM(IF(a.Matric = m.Matric_arbitre_secondaire,1,0)) Secondaire, 
+    COUNT(a.Matric) Total 
+FROM `gickp_Liste_Coureur` lc, `gickp_Journees` j, `gickp_Arbitre` a, `gickp_Matchs` m 
+WHERE 1=1
+AND m.Id_journee = j.Id
+AND a.Matric = lc.Matric
+AND lc.Numero_comite_reg != 98
+AND (j.Code_competition LIKE 'N%' 
+       OR j.Code_competition LIKE 'CF%')
+AND a.saison = 2020
+AND (a.Matric = m.Matric_arbitre_principal
+	OR a.Matric = m.Matric_arbitre_secondaire)
+
+GROUP BY lc.Matric
+ORDER BY Nom, Prenom, Matric
+
+/**
+ * Nombre d'arbitres ayant officié sur une compétition nationale 
+ * (arbitre principal ou secondaire) 
+ *
+ * Author:  Laurent
+ * Created: 25 jan. 2021
+ * Updated: 25 jan. 2021
+ */
