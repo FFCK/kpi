9c0d40ebc10e34c5f9bae32730a28b8a79a49bb1
laurentgarrigue <lgarrigue@gmail.com>
2021-01-28 00:14:08 +0100

    Feat: stats arbitres

--- SQL/activite_arbitres.sql
+++ SQL/activite_arbitres.sql
@@ -29,30 +29,29 @@ ORDER BY Nom, Prenom, Matric, Saison, Competition
  * Liste arbitres ayant officié sur une compétition nationale 
  * (arbitre principal ou secondaire)
  *
- * !!!!! REQUETE INCORRECTE !!!!!
- *
  * Author:  Laurent
  * Created: 25 jan. 2021
- * Updated: 25 jan. 2021
+ * Updated: 27 jan. 2021
  */
- SELECT lc.Matric, lc.Nom, lc.Prenom, lc.Sexe, lc.Naissance, lc.Numero_club Club,
+SELECT lc.Numero_club Club, c.Libelle, lc.Matric, lc.Nom, lc.Prenom, lc.Sexe, lc.Naissance, 
 	a.Arb, a.niveau, a.saison annee, 
 	SUM(IF(a.Matric = m.Matric_arbitre_principal,1,0)) Principal, 
     SUM(IF(a.Matric = m.Matric_arbitre_secondaire,1,0)) Secondaire, 
     COUNT(a.Matric) Total 
-FROM `gickp_Liste_Coureur` lc, `gickp_Journees` j, `gickp_Arbitre` a, `gickp_Matchs` m 
+FROM `gickp_Liste_Coureur` lc, `gickp_Journees` j, `gickp_Arbitre` a, 
+    `gickp_Matchs` m, `gickp_Club` c 
 WHERE 1=1
 AND m.Id_journee = j.Id
 AND a.Matric = lc.Matric
+AND lc.Numero_club = c.Code
 AND lc.Numero_comite_reg != 98
 AND (j.Code_competition LIKE 'N%' 
        OR j.Code_competition LIKE 'CF%')
-AND a.saison = 2020
+AND j.Code_saison = 2020
 AND (a.Matric = m.Matric_arbitre_principal
 	OR a.Matric = m.Matric_arbitre_secondaire)
-
 GROUP BY lc.Matric
-ORDER BY Nom, Prenom, Matric
+ORDER BY Club, Nom, Prenom, Matric
 
 /**
  * Nombre d'arbitres ayant officié sur une compétition nationale 
@@ -60,5 +59,23 @@ ORDER BY Nom, Prenom, Matric
  *
  * Author:  Laurent
  * Created: 25 jan. 2021
- * Updated: 25 jan. 2021
+ * Updated: 27 jan. 2021
  */
+SELECT lc.Numero_club Club, c.Libelle, 
+	SUM(IF(a.Matric = m.Matric_arbitre_principal,1,0)) Principal, 
+    SUM(IF(a.Matric = m.Matric_arbitre_secondaire,1,0)) Secondaire, 
+    COUNT(lc.Numero_club) Total 
+FROM `gickp_Liste_Coureur` lc, `gickp_Journees` j, `gickp_Arbitre` a,
+    `gickp_Matchs` m, `gickp_Club` c
+WHERE 1=1
+AND m.Id_journee = j.Id
+AND a.Matric = lc.Matric
+AND lc.Numero_club = c.Code
+AND lc.Numero_comite_reg != 98
+AND (j.Code_competition LIKE 'N%' 
+       OR j.Code_competition LIKE 'CF%')
+AND j.Code_saison = 2020
+AND (a.Matric = m.Matric_arbitre_principal
+	OR a.Matric = m.Matric_arbitre_secondaire)
+GROUP BY lc.Numero_club
+ORDER BY Club
