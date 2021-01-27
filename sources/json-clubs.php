bcb2f721a328e676e39b57f3832cf1cee52eaf22
laurentgarrigue <lgarrigue@gmail.com>
2021-01-27 22:48:33 +0100

    Fix: reactivation json-clubs

--- sources/json-clubs.php
+++ sources/json-clubs.php
@@ -7,20 +7,19 @@ $myBdd = new MyBdd();
 
 // Chargement des Clubs ayant une équipe inscrite dans une compétition de polo ...
 $arrayClub = array();
-$sql  = "SELECT DISTINCT c.Code, c.Libelle, c.Coord, c.Postal, c.www, c.email, "
-        . "cd.Libelle comitedep, cr.Libelle comitereg, "
-        . "GROUP_CONCAT(CONCAT_WS('|', e.Numero, e.Libelle) ORDER BY e.Libelle ASC SEPARATOR ';') Equipes "
-        . "FROM gickp_Club c, gickp_Equipe e, "
-        . "gickp_Comite_dep cd, gickp_Comite_reg cr "
-        . "WHERE c.Code = e.Code_club "
-//        . "AND c.Coord IS NOT NULL "
-//        . "AND c.Coord != '' "
-        . "AND c.Code_comite_dep = cd.Code "
-        . "AND cd.Code_comite_reg = cr.Code "
-        . "GROUP BY c.Code "
-        . "ORDER BY c.Officiel DESC, c.Code, c.Libelle ";
-$result = $myBdd->Query($sql);
-while ($row = $myBdd->FetchArray($result, $resulttype=MYSQL_ASSOC)){ 
+$sql  = "SELECT DISTINCT c.Code, c.Libelle, c.Coord, c.Postal, c.www, c.email, 
+    cd.Libelle comitedep, cr.Libelle comitereg, 
+    GROUP_CONCAT(CONCAT_WS('|', e.Numero, e.Libelle) 
+        ORDER BY e.Libelle ASC SEPARATOR ';') Equipes 
+    FROM gickp_Club c, gickp_Equipe e, 
+    gickp_Comite_dep cd, gickp_Comite_reg cr 
+    WHERE c.Code = e.Code_club 
+    AND c.Code_comite_dep = cd.Code 
+    AND cd.Code_comite_reg = cr.Code 
+    GROUP BY c.Code 
+    ORDER BY c.Officiel DESC, c.Code, c.Libelle ";
+$result = $myBdd->pdo->query($sql);
+while ($row = $result->fetch()){ 
     if($row['Coord'] == NULL) {
         $row['lat'] = "";
         $row['lon'] = "";
