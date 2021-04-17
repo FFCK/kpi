f64fc340b3ce09ff585f342bab23237a8aae789f
laurentgarrigue <lgarrigue@gmail.com>
2021-04-18 00:40:53 +0200

    Allègement Liste_Coureur

--- SQL/licencies_non_utilises.sql
+++ SQL/licencies_non_utilises.sql
@@ -1,32 +1,96 @@
--- Trop long
--- SELECT * FROM `gickp_Liste_Coureur` lc
--- LEFT OUTER JOIN gickp_Surclassements s ON (lc.Matric = s.Matric)
--- LEFT OUTER JOIN gickp_Rc rc ON (lc.Matric = rc.Matric)
--- LEFT OUTER JOIN gickp_Matchs_Joueurs mj ON (lc.Matric = mj.Matric)
--- LEFT OUTER JOIN gickp_Matchs_Detail md ON (lc.Matric = md.Competiteur)
--- LEFT OUTER JOIN gickp_Competitions_Equipes_Joueurs cej ON (lc.Matric = cej.Matric)
--- LEFT OUTER JOIN gickp_Arbitre a ON (lc.Matric = a.Matric)
--- LEFT OUTER JOIN gickp_Utilisateur u ON (lc.Matric = u.Code)
--- WHERE s.Matric IS NULL
--- AND rc.Matric IS NULL
--- AND mj.Matric IS NULL
--- AND md.Competiteur IS NULL
--- AND cej.Matric IS NULL
--- AND a.Matric IS NULL
--- AND u.Code IS NULL
--- AND lc.Origine = 2007
+-- ------------------- --
+-- EPURATION LICENCIES --
+-- ------------------- --
 
+-- Vidage table recherche
+DELETE FROM gickp_Recherche_Licence;
 
--- Mieux (en plusieurs fois)
+-- Epuration journal
+DELETE FROM gickp_Journal
+WHERE Dates < '2017-01-01';
+
+-- Création table temporaire
+CREATE TABLE gickp_Liste_Coureur_Legacy 
+    SELECT * FROM gickp_Liste_Coureur;
+
+-- gickp_Arbitre : 297586 -> 295548
 DELETE lc 
-FROM gickp_Liste_Coureur_reduite lc
-LEFT OUTER JOIN gickp_Arbitres a 
+FROM gickp_Liste_Coureur_Legacy lc
+LEFT OUTER JOIN gickp_Arbitre a 
 	ON (lc.Matric = a.Matric)
-WHERE a.Matric IS NOT NULL
--- (...)
+WHERE a.Matric IS NOT NULL;
 
+-- gickp_Competitions_Equipes_Joueurs : 295548 - 4151 => 291397
+DELETE lc 
+FROM gickp_Liste_Coureur_Legacy lc
+LEFT OUTER JOIN gickp_Competitions_Equipes_Joueurs a 
+	ON (lc.Matric = a.Matric)
+WHERE a.Matric IS NOT NULL;
+
+-- gickp_Matchs_Detail : 291386 - 128 => 291326
+DELETE lc 
+FROM gickp_Liste_Coureur_Legacy lc
+LEFT OUTER JOIN gickp_Matchs_Detail a 
+	ON (lc.Matric = a.Competiteur)
+WHERE a.Competiteur IS NOT NULL;
+
+-- gickp_Matchs_Joueurs : 291386 - 128 => 291326
+DELETE lc 
+FROM gickp_Liste_Coureur_Legacy lc
+LEFT OUTER JOIN gickp_Matchs_Joueurs a 
+	ON (lc.Matric = a.Matric)
+WHERE a.Matric IS NOT NULL;
+
+-- gickp_Rc : 291326
+DELETE lc 
+FROM gickp_Liste_Coureur_Legacy lc
+LEFT OUTER JOIN gickp_Rc a 
+	ON (lc.Matric = a.Matric)
+WHERE a.Matric IS NOT NULL;
+
+-- gickp_Surclassements : 291326 - 5 => 291321
+DELETE lc 
+FROM gickp_Liste_Coureur_Legacy lc
+LEFT OUTER JOIN gickp_Surclassements a 
+	ON (lc.Matric = a.Matric)
+WHERE a.Matric IS NOT NULL;
+
+-- gickp_Utilisateur : 291321 - 9 => 291312
+DELETE lc 
+FROM gickp_Liste_Coureur_Legacy lc
+LEFT OUTER JOIN gickp_Utilisateur a 
+	ON (lc.Matric = a.Code)
+WHERE a.Code IS NOT NULL;
+
+-- gickp_Journal : 291397 - 11 => 291386
+ALTER TABLE `gickp_Journal` ADD INDEX(`Users`); 
+ALTER TABLE `gickp_Liste_Coureur_Legacy` ADD PRIMARY KEY(`Matric`); 
+
+DELETE lc 
+FROM gickp_Liste_Coureur_Legacy lc
+LEFT OUTER JOIN gickp_Journal a 
+	ON (lc.Matric = a.Users)
+WHERE a.Users IS NOT NULL;
+
+-- gickp_Liste_Coureur_Legacy : conservation des 3 dernières saisons
+-- -121908 lignes => 169276
+DELETE  
+FROM gickp_Liste_Coureur_Legacy
+WHERE Origine >= '2018';
+
+
+
+
+
+-- Finalisation --
 DELETE lc 
 FROM gickp_Liste_Coureur lc
-LEFT OUTER JOIN gickp_Liste_Coureur_reduite r 
+LEFT OUTER JOIN gickp_Liste_Coureur_Legacy r 
 	ON (lc.Matric = r.Matric)
-WHERE r.Matric IS NOT NULL
+WHERE r.Matric IS NOT NULL;
+
+-- Optimisation
+OPTIMIZE TABLE `gickp_Liste_Coureur`; 
+
+-- Suppression table temporaire
+DROP TABLE gickp_Liste_Coureur_Legacy;
