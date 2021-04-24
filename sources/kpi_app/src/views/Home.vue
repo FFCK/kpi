550d105eac90843fd2c7bf8b560dcaa52ced61b1
laurentgarrigue <lgarrigue@gmail.com>
2021-04-24 16:48:21 +0200

    Feat: kpi_app

---
+++ sources/kpi_app/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" width="100" height="100">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home'
+}
+</script>
