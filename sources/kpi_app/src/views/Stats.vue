a01fb3d635c72205b10f9984bd270a12961fbad0
laurentgarrigue <lgarrigue@gmail.com>
2021-04-26 23:47:25 +0200

    Feat: main nav

---
+++ sources/kpi_app/src/views/Stats.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container-sm">
+    <h1>Stats</h1>
+    <span class="fas fa-chart-bar"></span>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Stats',
+  methods: {
+    changePage (pageName) {
+      this.$router.push({ name: pageName })
+    }
+  }
+}
+</script>
