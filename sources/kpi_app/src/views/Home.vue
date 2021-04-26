ec74e4fb41962fe855ee58628a9c81e849fd8fc7
laurentgarrigue <lgarrigue@gmail.com>
2021-04-26 23:47:25 +0200

    Feat: main nav

--- sources/kpi_app/src/views/Home.vue
+++ sources/kpi_app/src/views/Home.vue
@@ -1,10 +1,51 @@
 <template>
-  <div class="home">
-    <div>
+  <div class="container-sm">
+    <div class="row">
+      <div class="col-md-4">
+        <div class="card mb-2">
+          <div class="card-body">
+            <h5 class="card-title">
+              Games
+            </h5>
+            <p class="card-text">Game report</p>
+            <button class="btn btn-primary" @click="changePage('Game')">
+              Go
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card mb-2">
+          <div class="card-body">
+            <h5 class="card-title">
+              Stats
+            </h5>
+            <p class="card-text">Stats report</p>
+            <button class="btn btn-primary" @click="changePage('Stats')">
+              Go
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card mb-2">
+          <div class="card-body">
+            <h5 class="card-title">
+              Scrutineering
+            </h5>
+            <p class="card-text">Game report</p>
+            <button class="btn btn-primary" @click="changePage('Scrutineering')">
+              Go
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="mt-1">
       <img alt="Vue logo" src="../assets/logo.png" width="100" height="100">
     </div>
 
-    <button class="btn btn-primary" @click="changePage">About !</button>
   </div>
 </template>
 
@@ -14,8 +55,8 @@
 export default {
   name: 'Home',
   methods: {
-    changePage () {
-      this.$router.push({ name: 'About' })
+    changePage (pageName) {
+      this.$router.push({ name: pageName })
     }
   }
 }
---
+++ sources/kpi_app/src/views/Scrutineering.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container-sm">
+    <h1>Scrutineering</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Scrutineering',
+  methods: {
+    changePage (pageName) {
+      this.$router.push({ name: pageName })
+    }
+  }
+}
+</script>
