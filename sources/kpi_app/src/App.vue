b0de7d5f362f673b9db54c5133d3f969d310bb1e
laurentgarrigue <lgarrigue@gmail.com>
2021-04-27 21:24:27 +0200

    Feat: transition

--- sources/kpi_app/src/App.vue
+++ sources/kpi_app/src/App.vue
@@ -3,7 +3,9 @@
         <Nav />
 
         <br class="mt-4 mb-3" />
-        <router-view />
+        <transition name="fade">
+          <router-view />
+        </transition>
     </div>
 </template>
 
@@ -30,5 +32,16 @@ export default {
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.3s ease-out;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
 }
 </style>
