402f4cf7d28e7aeb51f444839ed4a995bb0b70d8
laurentgarrigue <lgarrigue@gmail.com>
2021-04-27 14:33:26 +0200

    Feat: i18n

--- sources/kpi_app/src/App.vue
+++ sources/kpi_app/src/App.vue
@@ -1,87 +1,21 @@
 <template>
     <div>
-        <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#">
-                <img
-                    src="./assets/logo.png"
-                    width="30"
-                    height="30"
-                    alt="logo"
-                />
-            </a>
-            <button
-                class="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbar"
-                aria-controls="navbar"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-            >
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbar">
-                <ul class="navbar-nav mr-auto my-2 my-lg-0 navbar-nav-scroll">
-                    <li class="nav-item">
-                        <router-link class="nav-link" to="/">
-                            Home<span class="sr-only">(current)</span>
-                        </router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link class="nav-link" to="/game">
-                            Game
-                        </router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link class="nav-link" to="/stats">
-                            Stats
-                        </router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link class="nav-link" to="/scrutineering">
-                            Scrutineering
-                        </router-link>
-                    </li>
-                    <!-- <li class="nav-item dropdown">
-                        <a
-                            class="nav-link dropdown-toggle"
-                            href="#"
-                            id="navbarScrollingDropdown"
-                            role="button"
-                            data-toggle="dropdown"
-                            aria-expanded="false"
-                        >
-                            Link
-                        </a>
-                        <ul
-                            class="dropdown-menu"
-                            aria-labelledby="navbarScrollingDropdown"
-                        >
-                            <li>
-                                <a class="dropdown-item" href="#">Action</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="#"
-                                    >Another action</a
-                                >
-                            </li>
-                            <li><hr class="dropdown-divider" /></li>
-                            <li>
-                                <a class="dropdown-item" href="#"
-                                    >Something else here</a
-                                >
-                            </li>
-                        </ul>
-                    </li> -->
-                </ul>
-            </div>
-        </nav>
+        <Nav />
 
         <br class="mt-4 mb-3" />
         <router-view />
     </div>
 </template>
+
+<script>
+import Nav from '@/components/Nav'
+
+export default {
+  components: {
+    Nav
+  }
+}
+</script>
 
 <style lang="scss">
 @import "./assets/styles/custom.scss";
@@ -96,9 +30,5 @@
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-}
-
-.navbar a.nav-link.router-link-active {
-    color: #ffffff;
 }
 </style>
