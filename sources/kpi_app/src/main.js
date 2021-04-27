d3e0b8387b5ffd998ca9e1755ded48144756c713
laurentgarrigue <lgarrigue@gmail.com>
2021-04-27 21:24:27 +0200

    Feat: transition

--- sources/kpi_app/src/main.js
+++ sources/kpi_app/src/main.js
@@ -1,12 +1,10 @@
 import { createApp } from 'vue'
-// import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import 'jquery/src/jquery.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
-// import VueI18n from 'vue-i18n'
 import i18n from './i18n'
 
 createApp(App).use(store).use(router).use(i18n).mount('#app')
