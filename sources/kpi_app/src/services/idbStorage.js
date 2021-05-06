cd4906d4e07c4dcbf6c41df3f633431bcf77277d
laurentgarrigue <lgarrigue@gmail.com>
2021-05-06 23:08:36 +0200

    Feat: VueX-ORM & plugin Axios & IndexedDB persist

---
+++ sources/kpi_app/src/services/idbStorage.js
@@ -0,0 +1,100 @@
+import { openDB } from 'idb'
+
+const dbPromise = _ => {
+  if (!('indexedDB' in window)) {
+    throw new Error('Browser does not support IndexedDB')
+  }
+
+  return openDB('kpi', 3, {
+    // mise à jour de la structure de base en fonction de la version
+    upgrade: (db, oldVersion, newVersion, transaction) => {
+      switch (oldVersion) {
+        case 0:
+          upgradeDBfromV0toV3()
+          break
+        case 1:
+          upgradeDBfromV1toV3()
+          break
+        case 2:
+          upgradeDBfromV2toV3()
+          break
+        default:
+          console.error('unknown db version')
+      }
+
+      function upgradeDBfromV0toV3 () {
+        db.createObjectStore('preferences', { keyPath: 'id' })
+        db.createObjectStore('users', { keyPath: 'id' })
+        db.createObjectStore('Photo', { keyPath: 'id' })
+      }
+
+      function upgradeDBfromV1toV3 () {
+        db.createObjectStore('users', { keyPath: 'id' })
+        db.createObjectStore('Photo', { keyPath: 'id' })
+      }
+
+      function upgradeDBfromV2toV3 () {
+        db.createObjectStore('Photo', { keyPath: 'id' })
+      }
+    }
+  })
+}
+
+const dbCount = async (storeName) => {
+  try {
+    const db = await dbPromise()
+    return db.count(storeName)
+  } catch (error) {
+    return error
+  }
+}
+
+const dbFind = async (storeName, id) => {
+  try {
+    const db = await dbPromise()
+    return db.get(storeName, id)
+  } catch (error) {
+    return error
+  }
+}
+
+const dbFindAll = async storeName => {
+  try {
+    const db = await dbPromise()
+    return db.getAll(storeName)
+  } catch (error) {
+    return error
+  }
+}
+
+const dbPut = async (storeName, tasks) => {
+  try {
+    const db = await dbPromise()
+    const tx = db.transaction(storeName, 'readwrite')
+    const store = tx.objectStore(storeName)
+    store.put(tasks)
+    return tx.done
+  } catch (error) {
+    return error
+  }
+}
+
+const dbDelete = async (storeName, id) => {
+  try {
+    const db = await dbPromise()
+    const tx = db.transaction(storeName, 'readwrite')
+    const store = tx.objectStore(storeName)
+    store.delete(id)
+    return id
+  } catch (error) {
+    return error
+  }
+}
+
+export default {
+  dbCount,
+  dbFind,
+  dbFindAll,
+  dbPut,
+  dbDelete
+}
