753f862d79bd9f9a0002e93fe18654ca055bf318
laurentgarrigue <lgarrigue@gmail.com>
2021-05-12 09:53:03 +0200

    Feat: Axios & VuexORMAxios config

--- sources/kpi_app/src/store/database/index.js
+++ sources/kpi_app/src/store/database/index.js
@@ -1,9 +1,19 @@
 import axios from 'axios'
+import { CUSTOM_AUTH_TOKEN } from '@/services/axiosInstance'
 import VuexORM from '@vuex-orm/core'
 import VuexORMAxios from '@vuex-orm/plugin-axios'
 import Photo from '@/store/models/Photo'
 
-VuexORM.use(VuexORMAxios, { axios })
+VuexORM.use(VuexORMAxios, {
+  axios,
+  baseURL: process.env.VUE_APP_API_BASE_URL,
+  headers: {
+    Accept: 'application/json',
+    'X-Requested-With': 'XMLHttpRequest',
+    'Content-Type': 'application/json',
+    Authorization: CUSTOM_AUTH_TOKEN
+  }
+})
 
 const database = new VuexORM.Database()
 
