1de15f7fab8ddd0f60fb5f5af482d81acad6d341
laurentgarrigue <lgarrigue@gmail.com>
2021-05-27 07:44:26 +0200

    Feat: scroll top

--- sources/kpi_app/src/services/mixins.js
+++ sources/kpi_app/src/services/mixins.js
@@ -44,6 +44,10 @@ const prefsMixin = {
           idbs.dbPut('preferences', Preferences.query().first())
         }
       }
+    },
+    scrollTop () {
+      document.body.scrollTop = 0 // For Safari
+      document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
     }
   },
   created () {
