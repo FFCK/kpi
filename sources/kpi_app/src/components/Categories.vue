65962684723da1561f8f5f027f28fc03fc86f770
laurentgarrigue <lgarrigue@gmail.com>
2021-05-27 07:34:45 +0200

    Feat: game filter & dates i18n

---
+++ sources/kpi_app/src/components/Categories.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+// import idbs from '@/services/idbStorage'
+// import Categories from '@/store/models/Categories'
+
+export default {
+  name: 'EventSelector',
+  data () {
+  },
+  methods: {
+  }
+}
+</script>
--- sources/kpi_app/src/components/design/Title.vue
+++ sources/kpi_app/src/components/design/Title.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>
+    <h1 class="shadow-sm">
       <i>{{ text }}</i>
     </h1>
   </div>
