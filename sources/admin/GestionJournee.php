7ab3167144c12ebf4c36330820e42326e96e13a8
laurentgarrigue <lgarrigue@gmail.com>
2021-06-03 23:16:58 +0200

    Fix: Match Equipe < 1 & !null

--- sources/admin/GestionJournee.php
+++ sources/admin/GestionJournee.php
@@ -472,21 +472,23 @@ class GestionJournee extends MyPageSecure
 			
 			while ($row = $result->fetch()) {				
 				$jour = $row['Date_match'];
-                if (utyGetSession('lang') == 'fr') {
-    				$listeJours[$jour] = utyDateUsToFr($jour);
+				if (utyGetSession('lang') == 'fr') {
+					$listeJours[$jour] = utyDateUsToFr($jour);
 					$row['Date_match'] = utyDateUsToFr($row['Date_match']);
-                } else {
-    				$listeJours[$jour] = $jour;
-                }
+				} else {
+					$listeJours[$jour] = $jour;
+				}
+				$EquipesAffectAuto = [];
 				if ($filtreJour == '' || $jour == $filtreJour) {
-                    if ($row['Libelle'] != '') {
-                        if (utyGetSession('lang') == 'en') {
-                            $EquipesAffectAuto = utyEquipesAffectAuto($row['Libelle']);
-                        } else {
-                            $EquipesAffectAuto = utyEquipesAffectAutoFR($row['Libelle']);
-                        }
+					if ($row['Libelle'] != '') {
+						if (utyGetSession('lang') == 'en') {
+							$EquipesAffectAuto = utyEquipesAffectAuto($row['Libelle']);
+						} else {
+							$EquipesAffectAuto = utyEquipesAffectAutoFR($row['Libelle']);
+						}
 					}
-
+					// if ($row['EquipeA'] == null) { $row['EquipeA'] = ''; }
+					// if ($row['EquipeB'] == null) { $row['EquipeB'] = ''; }
 					if (($row['EquipeA'] == '') && isset($EquipesAffectAuto[0]) && $EquipesAffectAuto[0] != '')
 						$row['EquipeA'] = $EquipesAffectAuto[0];
 					if ($row['EquipeB'] == '' && isset($EquipesAffectAuto[1]) && $EquipesAffectAuto[1] != '')
@@ -761,7 +763,7 @@ class GestionJournee extends MyPageSecure
 	{
 		$myBdd = $this->myBdd;
         
-        $idMatch = utyGetSession('idMatch', 0);
+		$idMatch = utyGetSession('idMatch', 0);
 		
 		$_SESSION['Intervalle_match'] = utyGetPost('Intervalle_match', utyGetSession('Intervalle_match', 40));
 		
@@ -776,6 +778,13 @@ class GestionJournee extends MyPageSecure
 		
 		$idEquipeA = (int) utyGetPost('idEquipeA', -1);
 		$idEquipeB = (int) utyGetPost('idEquipeB', -1);
+		
+		if ($idEquipeA < 1) {
+			$idEquipeA = null;
+		}
+		if ($idEquipeB < 1) {
+			$idEquipeB = null;
+		}
 	
 		$arbitre1 = trim(utyGetPost('arbitre1', ''));
 		if (strlen($arbitre1) == 0)
@@ -876,32 +885,38 @@ class GestionJournee extends MyPageSecure
 		$idEquipeA = (int) utyGetPost('idEquipeA', -1);
 		$idEquipeB = (int) utyGetPost('idEquipeB', -1);
 		
+		if ($idEquipeA < 1) {
+			$idEquipeA = null;
+		}
+		if ($idEquipeB < 1) {
+			$idEquipeB = null;
+		}
 		$arbitre1 = trim(utyGetPost('arbitre1', ''));
 		if (strlen($arbitre1) == 0) {
-            $arbitre1 = trim(utyGetPost('comboarbitre1', ''));
-        }
-        $arbitre1_matric = (int) utyGetPost('arbitre1_matric', -1);
+			$arbitre1 = trim(utyGetPost('comboarbitre1', ''));
+		}
+		$arbitre1_matric = (int) utyGetPost('arbitre1_matric', -1);
 					
 		$arbitre2 = trim(utyGetPost('arbitre2', ''));
 		if (strlen($arbitre2) == 0) {
-            $arbitre2 = trim(utyGetPost('comboarbitre2', ''));
-        }
-        $arbitre2_matric = (int) utyGetPost('arbitre2_matric', -1);
+			$arbitre2 = trim(utyGetPost('comboarbitre2', ''));
+		}
+		$arbitre2_matric = (int) utyGetPost('arbitre2_matric', -1);
 		
 		$coeffA = (float) utyGetPost('coeffA', 1);
 		if (strlen($coeffA) == 0) {
-            $coeffA = 1.0;
-        }
+			$coeffA = 1.0;
+		}
 
-        $coeffB = (float) utyGetPost('coeffB', 1);
+		$coeffB = (float) utyGetPost('coeffB', 1);
 		if (strlen($coeffB) == 0) {
-            $coeffB = 1.0;
-        }
+			$coeffB = 1.0;
+		}
 
-        if ($idJournee != 0) {
+		if ($idJournee != 0) {
 			if (strlen($numMatch) == 0) {
-                $numMatch = $this->LastNumeroOrdre($idJournee) + 1;
-            }
+				$numMatch = $this->LastNumeroOrdre($idJournee) + 1;
+			}
 
 			try {  
 				$myBdd->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
