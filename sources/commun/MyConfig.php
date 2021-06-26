e9892ef23be60d074d7f6a9f833d5751a897cca6
laurentgarrigue <lgarrigue@gmail.com>
2021-06-27 00:59:13 +0200

    Fix: Ajout titulaire déjà présent

--- sources/commun/MyConfig.php
+++ sources/commun/MyConfig.php
@@ -1,6 +1,6 @@
 <?php
 // Configuration Generale 
-define('NUM_VERSION','5.2.2');
+define('NUM_VERSION','5.2.3');
 
 // Décalage horaire -35 minutes pour affichage des prochains matchs + match courant (kpmatchs.php)
 define('DECALAGE_MINUTES', '-395 minutes');
