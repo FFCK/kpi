74aebfda807b1cd739c57b35e0405502ebb7ce65
laurentgarrigue <lgarrigue@gmail.com>
2021-08-24 10:23:19 +0200

    Feat: optimisation import element plus

--- sources/kpi_app/src/components/design/Navbar.vue
+++ sources/kpi_app/src/components/design/Navbar.vue
@@ -2,17 +2,14 @@
   <div>
     <nav class="navbar fixed-top navbar-expand-md navbar-dark bg-dark">
       <div class="container-fluid">
-        <router-link
-          to="/"
-          class="navbar-brand"
-        >
+        <router-link to="/" class="navbar-brand">
           <img
             src="@/assets/logo.png"
             width="30"
             height="30"
             alt="logo"
             class="d-inline-block align-middle"
-          >
+          />
           KPI App
         </router-link>
         <button
@@ -26,34 +23,22 @@
         >
           <span class="navbar-toggler-icon" />
         </button>
-        <div
-          id="navbarToggler"
-          class="collapse navbar-collapse"
-        >
+        <div id="navbarToggler" class="collapse navbar-collapse">
           <ul class="navbar-nav me-auto">
             <li class="nav-item">
-              <router-link
-                to="/"
-                class="nav-link text-nowrap active"
-              >
+              <router-link to="/" class="nav-link text-nowrap active">
                 <i class="bi bi-house" />
                 {{ $t("nav.Home") }}
               </router-link>
             </li>
             <li class="nav-item">
-              <router-link
-                to="/games"
-                class="nav-link text-nowrap"
-              >
+              <router-link to="/games" class="nav-link text-nowrap">
                 <i class="bi bi-list-ol" />
                 {{ $t("nav.Games") }}
               </router-link>
             </li>
             <li class="nav-item">
-              <router-link
-                to="/chart"
-                class="nav-link text-nowrap"
-              >
+              <router-link to="/chart" class="nav-link text-nowrap">
                 <i class="bi bi-diagram-2" />
                 {{ $t("nav.Chart") }}
               </router-link>
@@ -74,62 +59,32 @@
                 class="dropdown-menu bg-dark"
                 aria-labelledby="navbarDropdown"
               >
-                <li
-                  v-if="user"
-                  class="nav-item"
-                >
-                  <router-link
-                    to="/login"
-                    class="nav-link text-nowrap"
-                  >
+                <li v-if="user" class="nav-item">
+                  <router-link to="/login" class="nav-link text-nowrap">
                     <i class="bi bi-person-circle" />
                     {{ $t("nav.MyAccount") }}
                   </router-link>
                 </li>
-                <li
-                  v-else
-                  class="nav-item"
-                >
-                  <router-link
-                    to="/login"
-                    class="nav-link text-nowrap"
-                  >
+                <li v-else class="nav-item">
+                  <router-link to="/login" class="nav-link text-nowrap">
                     <i class="bi bi-person-circle" />
                     {{ $t("nav.Login") }}
                   </router-link>
                 </li>
-                <li
-                  v-if="user"
-                  class="nav-item"
-                >
-                  <router-link
-                    to="/game_report"
-                    class="nav-link text-nowrap"
-                  >
+                <li v-if="user" class="nav-item">
+                  <router-link to="/game_report" class="nav-link text-nowrap">
                     <i class="bi bi-clipboard" />
                     {{ $t("nav.GameReport") }}
                   </router-link>
                 </li>
-                <li
-                  v-if="user"
-                  class="nav-item"
-                >
-                  <router-link
-                    to="/stat_report"
-                    class="nav-link text-nowrap"
-                  >
+                <li v-if="user" class="nav-item">
+                  <router-link to="/stat_report" class="nav-link text-nowrap">
                     <i class="bi bi-clipboard-data" />
                     {{ $t("nav.StatReport") }}
                   </router-link>
                 </li>
-                <li
-                  v-if="user"
-                  class="nav-item"
-                >
-                  <router-link
-                    to="/scrutineering"
-                    class="nav-link text-nowrap"
-                  >
+                <li v-if="user" class="nav-item">
+                  <router-link to="/scrutineering" class="nav-link text-nowrap">
                     <i class="bi bi-clipboard-check" />
                     {{ $t("nav.Scrutineering") }}
                   </router-link>
@@ -137,15 +92,13 @@
               </ul>
             </li>
             <li class="nav-item">
-              <router-link
-                to="/about"
-                class="nav-link text-nowrap"
-              >
+              <router-link to="/about" class="nav-link text-nowrap">
                 <i class="el-icon-info" />
                 {{ $t("nav.About") }}
               </router-link>
             </li>
           </ul>
+
           <locale-switcher />
         </div>
       </div>
