c3a2e727bf2d70c74fdedf63f0f3d98e62355bde
laurentgarrigue <lgarrigue@gmail.com>
2021-08-30 13:59:57 +0200

    Feat: Tipeee

--- sources/kpi_app/src/views/About.vue
+++ sources/kpi_app/src/views/About.vue
@@ -1,22 +1,37 @@
 <template>
-  <div>
+  <div class="container">
     <title-component :text="$t('nav.About')" />
 
-    {{ version }}
+    <div class="text-center my-3">
+      <span class="btn btn-secondary">{{ version }}</span>
+    </div>
 
-    <coming-soon />
+    <div class="text-center">
+      Cette application vous plait ?<br />
+      Je la développe sur mon temps libre.<br />
+      Vous pouvez m'encourager à continuer et me soutenir sur Tipeee :
+    </div>
+
+    <div class="my-1 text-center">
+      <a href="https://fr.tipeee.com/lga" target="blank">
+        <img
+          alt="Vue logo"
+          src="../assets/bocal_tipeee.gif"
+          width="250"
+          height="250"
+        />
+      </a>
+    </div>
   </div>
 </template>
 
 <script>
 import TitleComponent from '@/components/design/Title'
-import ComingSoon from '@/components/design/ComingSoon.vue'
 
 export default {
   name: 'About',
   components: {
-    TitleComponent,
-    ComingSoon
+    TitleComponent
   },
   computed: {
     version () {
