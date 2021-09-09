5d9c5cf73537b6e54baf513419c77f8b0b2c6f15
laurentgarrigue <lgarrigue@gmail.com>
2021-09-09 18:45:57 +0200

    Fix: Event selector position

--- sources/app_dev/src/service-worker.js
+++ sources/app_dev/src/service-worker.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-const version = '0.9.3'
+const version = '0.9.5'
 importScripts('../third_party/workbox-v6.1.5/workbox-sw.js')
 workbox.setConfig({
   modulePathPrefix: '../third_party/workbox-v6.1.5',
