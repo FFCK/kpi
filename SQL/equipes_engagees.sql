d727e26144ebf42edf58c2d955e5580657faf8a7
laurentgarrigue <lgarrigue@gmail.com>
2021-11-11 23:14:13 +0100

    Fix: competitor stats

--- SQL/equipes_engagees.sql
+++ SQL/equipes_engagees.sql
@@ -2,25 +2,26 @@
 * Equipes engagées en compétition nationale
 */
 SELECT DISTINCT e.Libelle
-FROM gickp_Equipe e 
-LEFT JOIN gickp_Competitions_Equipes ce ON (e.Numero = ce.Numero)
+FROM kp_equipe e 
+LEFT JOIN kp_competition_equipe ce ON (e.Numero = ce.Numero)
 WHERE (
     ce.Code_compet LIKE 'N%'
     OR ce.Code_compet LIKE 'CF%'
     )
-AND ce.Code_saison = 2020
+AND ce.Code_saison = 2021
 
 /**
 * Equipes engagées en compétition nationale et régionale
 */
 SELECT DISTINCT e.Libelle
-FROM gickp_Equipe e 
-LEFT JOIN gickp_Competitions_Equipes ce ON (e.Numero = ce.Numero)
+FROM kp_equipe e 
+LEFT JOIN kp_competition_equipe ce ON (e.Numero = ce.Numero)
 WHERE (
     ce.Code_compet LIKE 'N%'
     OR ce.Code_compet LIKE 'CF%'
     OR ce.Code_compet LIKE 'REG%'
     OR ce.Code_compet LIKE 'T-R%'
+    OR ce.Code_compet LIKE 'OPEN%'
     )
-AND ce.Code_saison = 2020
+AND ce.Code_saison = 2021
 
