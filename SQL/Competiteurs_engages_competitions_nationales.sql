a76f590b26f9c931e891d41de8e1ade23c61d077
laurentgarrigue <lgarrigue@gmail.com>
2021-11-11 23:14:13 +0100

    Fix: competitor stats

--- SQL/Competiteurs_engages_competitions_nationales.sql
+++ SQL/Competiteurs_engages_competitions_nationales.sql
@@ -7,25 +7,25 @@
  * Updated: 25 jan. 2021
  */
 
-SELECT gickp_Journees.Code_competition, gickp_Liste_Coureur.Matric, gickp_Liste_Coureur.Sexe, 
-      gickp_Liste_Coureur.Naissance, gickp_Liste_Coureur.Numero_club,
-      gickp_Liste_Coureur.Numero_comite_dept, gickp_Liste_Coureur.Numero_comite_reg, 
-      gickp_Matchs_Joueurs.Capitaine 
-FROM gickp_Journees, gickp_Matchs, gickp_Matchs_Joueurs, gickp_Liste_Coureur
+SELECT j.Code_competition, l.Matric, l.Sexe, 
+      l.Naissance, l.Numero_club,
+      l.Numero_comite_dept, l.Numero_comite_reg, 
+      mj.Capitaine 
+FROM kp_journee j, kp_match m, kp_match_joueur mj, kp_licence l
 WHERE (
-    gickp_Journees.Code_competition LIKE 'N%' 
-       OR gickp_Journees.Code_competition LIKE 'CF%'
+    j.Code_competition LIKE 'N%' 
+       OR j.Code_competition LIKE 'CF%'
 --       OR gickp_Journees.Code_competition LIKE 'OPEN%'
 --       OR gickp_Journees.Code_competition LIKE 'REG%'
 --       OR gickp_Journees.Code_competition LIKE 'T-R%'
       )
-AND gickp_Liste_Coureur.Matric < 2000000
+AND l.Matric < 2000000
 -- AND gickp_Liste_Coureur.Sexe = "F"
-AND gickp_Journees.Code_saison = 2020
-AND gickp_Matchs.Id_journee = gickp_Journees.Id
-AND gickp_Matchs_Joueurs.Id_match = gickp_Matchs.Id
-AND gickp_Liste_Coureur.Matric = gickp_Matchs_Joueurs.Matric
-GROUP BY gickp_Liste_Coureur.Matric
+AND j.Code_saison = 2021
+AND m.Id_journee = j.Id
+AND mj.Id_match = m.Id
+AND l.Matric = mj.Matric
+GROUP BY l.Matric;
 
 /**
  * Nombre compétiteurs FFCK engagés dans des compétitions nationales
