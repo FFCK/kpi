38e9b52fb636bcec3dc0bade047095d86505a918
laurentgarrigue <lgarrigue@gmail.com>
2021-12-09 10:02:39 +0100

    Fix: query typo

--- sources/admin/v2/saveComments.php
+++ sources/admin/v2/saveComments.php
@@ -1,13 +1,13 @@
-<?php 
+<?php
 // prevent direct access *****************************************************
-$isAjax = isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND
-strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest';
-if(!$isAjax) {
-  $user_error = 'Access denied - not an AJAX request...';
-  trigger_error($user_error, E_USER_ERROR);
+$isAjax = isset($_SERVER['HTTP_X_REQUESTED_WITH']) and
+	strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest';
+if (!$isAjax) {
+	$user_error = 'Access denied - not an AJAX request...';
+	trigger_error($user_error, E_USER_ERROR);
 }
 // ***************************************************************************
-	
+
 include_once('../../commun/MyBdd.php');
 include_once('../../commun/MyTools.php');
 
@@ -21,7 +21,7 @@ $heure_fin_match = trim(utyGetPost('heure_fin_match', ''));
 $myBdd->AutorisationMatch($idMatch);
 
 if ($heure_fin_match != '') {
-	$heure_fin_match = '00:'.substr($heure_fin_match,-5,2).':'.substr($heure_fin_match,-2);
+	$heure_fin_match = '00:' . substr($heure_fin_match, -5, 2) . ':' . substr($heure_fin_match, -2);
 	$sql = "UPDATE kp_match 
 		SET Commentaires_officiels = ?, 
 		Heure_fin = ? 
@@ -30,10 +30,9 @@ if ($heure_fin_match != '') {
 	$result->execute(array($value, $heure_fin_match, $idMatch));
 } else {
 	$sql = "UPDATE kp_match 
-		SET Commentaires_officiels = ?, 
+		SET Commentaires_officiels = ? 
 		WHERE Id = ? ";
 	$result = $myBdd->pdo->prepare($sql);
 	$result->execute(array($value, $idMatch));
 }
 echo $value;
-
