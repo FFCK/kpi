1670cafe0f31afde763685500f86c90ca14b4129
laurentgarrigue <lgarrigue@gmail.com>
2021-12-10 11:34:54 +0100

    Fix: notice

--- sources/admin/v2/ajax_updateChrono.php
+++ sources/admin/v2/ajax_updateChrono.php
@@ -1,13 +1,13 @@
-<?php 
+<?php
 // prevent direct access *****************************************************
-$isAjax = isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND
-strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest';
-if(!$isAjax) {
-  $user_error = 'Access denied - not an AJAX request...';
-  trigger_error($user_error, E_USER_ERROR);
+$isAjax = isset($_SERVER['HTTP_X_REQUESTED_WITH']) and
+	strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest';
+if (!$isAjax) {
+	$user_error = 'Access denied - not an AJAX request...';
+	trigger_error($user_error, E_USER_ERROR);
 }
 // ***************************************************************************
-	
+
 include_once('../../commun/MyBdd.php');
 include_once('../../commun/MyTools.php');
 include_once('../../live/create_cache_match.php');
@@ -19,26 +19,17 @@ $idMatch = (int) utyGetPost('idMatch');
 $start_time = trim(utyGetPost('start_time'));
 $run_time = trim(utyGetPost('run_time'));
 
-if ($action == 'RAZ'){
-	$sql = "DELETE FROM kp_chrono 
-		WHERE IdMatch = ? ";
-	$result = $myBdd->pdo->prepare($sql);
-	$result->execute(array($idMatch));
-} else {
-	$start_time_server = time()%86400; 	// COSANDCO : Prise en compte de l'heure du Serveur ...
-	$sql = "UPDATE kp_chrono 
-		SET start_time = ?, 
-		start_time_server = ?, 
-		run_time = ? 
-		WHERE IdMatch = ? ";
-	$result = $myBdd->pdo->prepare($sql);
-	$result->execute(array($start_time, $start_time_server, $run_time, $idMatch));
-}
+$start_time_server = time() % 86400; 	// COSANDCO : Prise en compte de l'heure du Serveur ...
+$sql = "UPDATE kp_chrono 
+	SET start_time = ?, 
+	start_time_server = ?, 
+	run_time = ? 
+	WHERE IdMatch = ? ";
+$result = $myBdd->pdo->prepare($sql);
+$result->execute(array($start_time, $start_time_server, $run_time, $idMatch));
 
 // COSANDCO : Creation du Cache ...
 $cMatch = new CacheMatch($_GET);
 $cMatch->MatchChrono($myBdd, $idMatch);
 
-echo "OK"; 
-
-    
+echo "OK";
