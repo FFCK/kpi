79296743b02b8c5ce67803d9cd0b53ade683fc8d
laurentgarrigue <lgarrigue@gmail.com>
2021-12-14 14:45:32 +0100

    Refactor: Live score vanilla js & animations

--- sources/live/js/tv.js
+++ sources/live/js/tv.js
@@ -1,197 +1,205 @@
-var theContext = new Object();
-theContext.scenario_row = 0;
-theContext.scenario_url = "";
-theContext.scenario_duree_max = 0;
-theContext.scenario_duree = 0;
+var theContext = new Object()
+theContext.scenario_row = 0
+theContext.scenario_url = ""
+theContext.scenario_duree_max = 0
+theContext.scenario_duree = 0
 
-function Go_list_medals()
-{
-	var param;
-	param  = "show=list_medals";
-	param += "&voie="+$('#list_medals_channel').val();
-	param += "&competition="+$('#list_medals_competition').val();
-	Go(param);
+function Go_list_medals () {
+	var param
+	param = "show=list_medals"
+	param += "&voie=" + document.querySelector('#list_medals_channel').value
+	param += "&competition=" + document.querySelector('#list_medals_competition').value
+	Go(param)
 }
 
-function Go_referee()
-{
-	var param;
-	param  = "show=referee";
-	param += "&voie="+$('#referee_channel').val();
-	param += "&match="+$('#referee_match').val();
-	Go(param);
+function Go_referee () {
+	var param
+	param = "show=referee"
+	param += "&voie=" + document.querySelector('#referee_channel').value
+	param += "&match=" + document.querySelector('#referee_match').value
+	Go(param)
 }
 
-function Go_player()
-{
-	var param;
-	param  = "show=player";
-	param += "&voie="+$('#player_channel').val();
-	param += "&match="+$('#player_match').val();
-	param += "&team="+$('#player_team').val();
-	param += "&number="+$('#player_number').val();
-	Go(param);
+function Go_player () {
+	var param
+	param = "show=player"
+	param += "&voie=" + document.querySelector('#player_channel').value
+	param += "&match=" + document.querySelector('#player_match').value
+	param += "&team=" + document.querySelector('#player_team').value
+	param += "&number=" + document.querySelector('#player_number').value
+	Go(param)
 }
 
-function Go_player_medal()
-{
-	var param;
-	param  = "show=player_medal";
-	param += "&voie="+$('#player_medal_channel').val();
-	param += "&match="+$('#player_medal_match').val();
-	param += "&team="+$('#player_medal_team').val();
-	param += "&number="+$('#player_medal_number').val();
-	param += "&medal="+$('#player_medal_medal').val();
-	Go(param);
+function Go_player_medal () {
+	var param
+	param = "show=player_medal"
+	param += "&voie=" + document.querySelector('#player_medal_channel').value
+	param += "&match=" + document.querySelector('#player_medal_match').value
+	param += "&team=" + document.querySelector('#player_medal_team').value
+	param += "&number=" + document.querySelector('#player_medal_number').value
+	param += "&medal=" + document.querySelector('#player_medal_medal').value
+	Go(param)
 }
 
-function Go_team()
-{
-	var param;
-	param  = "show=team";
-	param += "&voie="+$('#team_channel').val();
-	param += "&match="+$('#team_match').val();
-	param += "&team="+$('#team_team').val();
-	Go(param);
+function Go_team () {
+	var param
+	param = "show=team"
+	param += "&voie=" + document.querySelector('#team_channel').value
+	param += "&match=" + document.querySelector('#team_match').value
+	param += "&team=" + document.querySelector('#team_team').value
+	Go(param)
 }
 
-function Go_team_medal()
-{
-	var param;
-	param  = "show=team_medal";
-	param += "&voie="+$('#team_medal_channel').val();
-	param += "&match="+$('#team_medal_match').val();
-	param += "&team="+$('#team_medal_team').val();
-	param += "&medal="+$('#team_medal_medal').val();
-	Go(param);
+function Go_team_medal () {
+	var param
+	param = "show=team_medal"
+	param += "&voie=" + document.querySelector('#team_medal_channel').value
+	param += "&match=" + document.querySelector('#team_medal_match').value
+	param += "&team=" + document.querySelector('#team_medal_team').value
+	param += "&medal=" + document.querySelector('#team_medal_medal').value
+	Go(param)
 }
 
-function Go_match()
-{
-	var param;
-	param  = "show=match";
-	param += "&voie="+$('#match_channel').val();
-	param += "&match="+$('#match_match').val();
-	Go(param);
+function Go_match () {
+	var param
+	param = "show=match"
+	param += "&voie=" + document.querySelector('#match_channel').value
+	param += "&match=" + document.querySelector('#match_match').value
+	Go(param)
 }
 
-function Go_match_score()
-{
-	var param;
-	param  = "show=match_score";
-	param += "&voie="+$('#match_score_channel').val();
-	param += "&match="+$('#match_score_match').val();
-	Go(param);
+function Go_match_score () {
+	var param
+	param = "show=match_score"
+	param += "&voie=" + document.querySelector('#match_score_channel').value
+	param += "&match=" + document.querySelector('#match_score_match').value
+	Go(param)
 }
 
-function Go_list_team()
-{
-	var param;
-	param  = "show=list_team";
-	param += "&voie="+$('#list_team_channel').val();
-	param += "&match="+$('#list_team_match').val();
-	param += "&team="+$('#list_team_team').val();
-	Go(param);
+function Go_list_team () {
+	var param
+	param = "show=list_team"
+	param += "&voie=" + document.querySelector('#list_team_channel').value
+	param += "&match=" + document.querySelector('#list_team_match').value
+	param += "&team=" + document.querySelector('#list_team_team').value
+	Go(param)
 }
 
-function Go_presentation()
-{
-	ChangeVoie(+$('#list_presentation_channel').val(), $('#list_presentation_url').val());
+function Go_presentation () {
+	ChangeVoie(+document.querySelector('#list_presentation_channel').value, document.querySelector('#list_presentation_url').value)
 }
 
-function Go_raz()
-{
-	var param;
-	param  = "show=reset";
-	param += "&voie="+$('#list_team_channel').val();
-	Go(param);
+function Go_raz () {
+	var param
+	param = "show=reset"
+	param += "&voie=" + document.querySelector('#list_team_channel').value
+	Go(param)
 }
 
-function Go(param)
-{
-//    alert("ajax_change_tv.php?"+param);
-    $.ajax({ type: "POST", url: "ajax_change_tv.php", dataType: "html", data: param, cache: false, 
-                success: function(htmlData) {
-						alert(htmlData);
-				}
-	});
+function Go (param) {
+	// $.ajax({
+	// 	type: "POST", url: "ajax_change_tv.php", dataType: "html", data: param, cache: false,
+	// 	success: function (htmlData) {
+	// 		alert(htmlData)
+	// 	}
+	// })
+
+	axios({
+		method: 'post',
+		url: './ajax_change_tv.php',
+		params: param,
+		responseType: 'text'
+	})
+		.then(function (response) {
+			alert(response.data)
+		})
+		.catch(function (error) {
+			console.log(error)
+			return
+		})
+
 }
 
-function Go_scenario()
-{
-	Next_scenario();
-	setInterval(RefreshScenario, 1000);
+function Go_scenario () {
+	Next_scenario()
+	setInterval(RefreshScenario, 1000)
 }
 
-function Next_scenario()
-{
-	++theContext.scenario_row;
-	var url = $("#scenario_url"+theContext.scenario_row).val();
+function Next_scenario () {
+	++theContext.scenario_row
+	var url = document.querySelector("#scenario_url" + theContext.scenario_row).value
 	if ((url == '') || (url == "undefined"))
-		theContext.scenario_row = 1;
-	
-	theContext.scenario_url = $("#scenario_url"+theContext.scenario_row).val();
-	theContext.scenario_duree_max = parseInt($("#scenario_duree"+theContext.scenario_row).val(), 10);
-	theContext.scenario_duree = 0;
-	
-	var voie = $("#scenario_channel").val();
-	ChangeVoie(voie, theContext.scenario_url);
+		theContext.scenario_row = 1
+
+	theContext.scenario_url = document.querySelector("#scenario_url" + theContext.scenario_row).value
+	theContext.scenario_duree_max = parseInt(document.querySelector("#scenario_duree" + theContext.scenario_row).value, 10)
+	theContext.scenario_duree = 0
+
+	var voie = document.querySelector("#scenario_channel").value
+	ChangeVoie(voie, theContext.scenario_url)
 }
 
-function RefreshScenario()
-{
-	++theContext.scenario_duree;
-	$("#tv_message").html("<b>Scenario en cours : "+theContext.scenario_url+" => "+theContext.scenario_duree+"/"+theContext.scenario_duree_max+"sec</b>");
+function RefreshScenario () {
+	++theContext.scenario_duree
+	document.querySelector("#tv_message").innerHTML = "<b>Scenario en cours : " + theContext.scenario_url + " => " + theContext.scenario_duree + "/" + theContext.scenario_duree_max + "sec</b>"
 	if (theContext.scenario_duree > theContext.scenario_duree_max)
-		Next_scenario();
+		Next_scenario()
 }
 
-function Go_url_splitter()
-{
-	var url = '/live/splitter.php';
-	for (var i=1;i<10;i++)
-	{
-		var urlRow = $("#scenario_url"+i).val();
+function Go_url_splitter () {
+	var url = '/live/splitter.php'
+	for (var i = 1; i < 10; i++) {
+		var urlRow = document.querySelector("#scenario_url" + i).value
 		if ((urlRow == '') || (urlRow == "undefined"))
-			break;
-		
-		urlRow = urlRow.replace("?", "|Q|");
-		for (;;)
-		{	
-			var urlRow2 = urlRow.replace("&", "|A|");
-			if (urlRow2 == urlRow) break;
-			urlRow = urlRow2;
+			break
+
+		urlRow = urlRow.replace("?", "|Q|")
+		for (; ;) {
+			var urlRow2 = urlRow.replace("&", "|A|")
+			if (urlRow2 == urlRow) break
+			urlRow = urlRow2
 		}
-		
-		if (i==1)
-			url += "?";
+
+		if (i == 1)
+			url += "?"
 		else
-			url += "&";
-		
-		url += "frame"+i+"="+urlRow;
+			url += "&"
+
+		url += "frame" + i + "=" + urlRow
 	}
-	
-	$("#tv_message").html("<b>URL progression : "+url);
+
+	document.querySelector("#tv_message").innerHTML = "<b>URL progression : " + url
 }
 
 
-function Init(voie, intervalle=3000)
-{
-	SetVoie(voie, intervalle);
-	
+function Init (voie, intervalle = 3000) {
+	SetVoie(voie, intervalle)
+
 	// Mode "show=command"
-	$('#list_medals_btn').click( function () { Go_list_medals(); return false;});
-	$('#referee_btn').click( function () { Go_referee(); return false;});
-	$('#player_btn').click( function () { Go_player(); return false;});
-	$('#player_medal_btn').click( function () { Go_player_medal(); return false;});
-	$('#team_btn').click( function () { Go_team(); return false;});
-	$('#team_medal_btn').click( function () { Go_team_medal(); return false;});
-	$('#match_btn').click( function () { Go_match(); return false;});
-	$('#match_score_btn').click( function () { Go_match_score(); return false;});
-	$('#list_team_btn').click( function () { Go_list_team(); return false;});
-	$('#list_presentation_btn').click( function () { Go_presentation(); return false;});
-	$('#scenario_btn').click( function () { Go_scenario(); return false;});
-	$('#url_splitter').click( function () { Go_url_splitter(); return false;});
-	
-	$('#raz_btn').click( function () { Go_raz(); return false;});
+	if (document.querySelector('#list_medals_btn'))
+		document.querySelector('#list_medals_btn').addEventListener('click', () => { Go_list_medals(); return false })
+	if (document.querySelector('#referee_btn'))
+		document.querySelector('#referee_btn').addEventListener('click', () => { Go_referee(); return false })
+	if (document.querySelector('#referee_btn'))
+		document.querySelector('#player_btn').addEventListener('click', () => { Go_player(); return false })
+	if (document.querySelector('#referee_btn'))
+		document.querySelector('#player_medal_btn').addEventListener('click', () => { Go_player_medal(); return false })
+	if (document.querySelector('#referee_btn'))
+		document.querySelector('#team_btn').addEventListener('click', () => { Go_team(); return false })
+	if (document.querySelector('#referee_btn'))
+		document.querySelector('#team_medal_btn').addEventListener('click', () => { Go_team_medal(); return false })
+	if (document.querySelector('#referee_btn'))
+		document.querySelector('#match_btn').addEventListener('click', () => { Go_match(); return false })
+	if (document.querySelector('#referee_btn'))
+		document.querySelector('#match_score_btn').addEventListener('click', () => { Go_match_score(); return false })
+	if (document.querySelector('#referee_btn'))
+		document.querySelector('#list_team_btn').addEventListener('click', () => { Go_list_team(); return false })
+	if (document.querySelector('#referee_btn'))
+		document.querySelector('#list_presentation_btn').addEventListener('click', () => { Go_presentation(); return false })
+	if (document.querySelector('#referee_btn'))
+		document.querySelector('#scenario_btn').addEventListener('click', () => { Go_scenario(); return false })
+	if (document.querySelector('#referee_btn'))
+		document.querySelector('#url_splitter').addEventListener('click', () => { Go_url_splitter(); return false })
+	if (document.querySelector('#referee_btn'))
+		document.querySelector('#raz_btn').addEventListener('click', () => { Go_raz(); return false })
 }	
--- sources/smarty/templates/frame_team.tpl
+++ sources/smarty/templates/frame_team.tpl
@@ -1,112 +1,113 @@
 {if $navGroup}
-    <div class="container-fluid categorie mb5">
-        <div class="col-md-12">
-            <a class="btn btn-primary" onclick="history.back()">
-                    < {#Retour#}
-            </a>
-        </div>
+  <div class="container-fluid categorie mb5">
+    <div class="col-md-12">
+      <a class="btn btn-primary" onclick="history.back()">
+        < {#Retour#} </a>
     </div>
+  </div>
 {/if}
 {if $Equipe > 0}
-    <div class="container-fluid flex">
-        <article class="col-md-6 padTopBottom">
-            <div class="form-horizontal">
-                <h2 class="col-sm-12 text-center" id="nomEquipe">{$nomEquipe}</h2>
-                <div class="form-group">
-                    <div class="col-sm-12 text-center" id="nomClub">
-                        <a class="btn btn-xs btn-default" {if !$arrayCompo}href='kpclubs.php?clubId={$Code_club}' title='{#Club#}'{/if}>
-                            {$Club}
-                        </a>
-                    </div>
-                </div>
-            </div>
-            {if $eColors}
-                <div class="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3" id="equipeColors">
-                    <a href="{$eColors}?v={$NUM_VERSION}" target="_blank">
-                        <img class="img-responsive img-thumbnail" src="{$eColors}?v={$NUM_VERSION}" alt="{$nomEquipe}">
-                    </a>
-                    <span class="pull-right badge">{$eSeason}</span>
-                </div>
-            {elseif $eLogo}
-                <div class="col-sm-6 col-sm-offset-3 col-xs-8 col-xs-offset-2" id="equipeColors">
-                    <a {if !$arrayCompo}href="kpclubs.php?clubId={$Code_club}" title='{#Club#}'{/if}>
-                        <img class="img-responsive img-thumbnail" src="{$eLogo}?v={$NUM_VERSION}" alt="{$nomEquipe}">
-                    </a>
-                </div>
-            {/if}
-        </article>
+  <div class="container-fluid flex">
+    <article class="col-md-6 padTopBottom">
+      <div class="form-horizontal">
+        <h2 class="col-sm-12 text-center" id="nomEquipe">{$nomEquipe}</h2>
+        <div class="form-group">
+          <div class="col-sm-12 text-center" id="nomClub">
+            <a class="btn btn-xs btn-default" {if !$arrayCompo}href='kpclubs.php?clubId={$Code_club}' title='{#Club#}'
+              {/if}>
+              {$Club}
+            </a>
+          </div>
+        </div>
+      </div>
+      {if $eColors}
+        <div class="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3" id="equipeColors">
+          <a href="{$eColors}?v={$NUM_VERSION}" target="_blank">
+            <img class="img-responsive img-thumbnail" src="{$eColors}?v={$NUM_VERSION}" alt="{$nomEquipe}">
+          </a>
+          <span class="pull-right badge">{$eSeason}</span>
+        </div>
+      {elseif $eLogo}
+        <div class="col-sm-6 col-sm-offset-3 col-xs-8 col-xs-offset-2" id="equipeColors">
+          <a {if !$arrayCompo}href="kpclubs.php?clubId={$Code_club}" title='{#Club#}' {/if}>
+            <img class="img-responsive img-thumbnail" src="{$eLogo}?v={$NUM_VERSION}" alt="{$nomEquipe}">
+          </a>
+        </div>
+      {/if}
+    </article>
 
-        <article class="col-md-6 padTopBottom" id="equipePalmares"> 
-            {if $codeCompet != ''}
-                <div class="page-header">
+    <article class="col-md-6 padTopBottom" id="equipePalmares">
+      {if $codeCompet != ''}
+        {* <div class="page-header">
                     <h4 class="text-info">{$recordCompetition.Soustitre}<br>{$recordCompetition.Soustitre2}</h4>
-                </div>
-                <table class='table table-condensed' id='tableStats'>
-                    <thead>
-                        <tr class='header'>
-                                <th class="text-center">#</th>
-                                <th>{#Nom#}</th>
-                                <th class="text-center">{#Buts#}</th>
-                                <th class="bg-success text-center">{#C_V#}</th>
-                                <th class="bg-warning text-center">{#C_J#}</th>
-                                <th class="bg-danger text-center">{#C_R#}</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {section name=i loop=$arrayCompo}
-                            <tr class='{cycle values="impair,pair"}'>
-                                <td class="text-center">
-                                    {if $arrayCompo[i].Capitaine == 'E'}
-                                        <span class="label label-default" title="Staff">S</span>
-                                    {else}
-                                        {$arrayCompo[i].Numero}
-                                    {/if}
-                                </td>
-                                <td>
-                                    {$arrayCompo[i].Nom|upper} {$arrayCompo[i].Prenom|upper}
-                                    {if $arrayCompo[i].Capitaine == 'C'}
-                                        <span class="label label-default" title="{#Capitaine#}">C</span>
-                                    {/if}
-                                </td>
-                                <td class="text-center">
-                                    {if $arrayCompo[i].buts > 0}
-                                        <span class="badgeg">{$arrayCompo[i].buts}</span>
-                                    {/if}
-                                </td>
-                                <td class="text-center">
-                                    {if $arrayCompo[i].verts > 0}
-                                        <span class="label label-success">{$arrayCompo[i].verts}</span>
-                                    {/if}
-                                </td>
-                                <td class="text-center">
-                                    {if $arrayCompo[i].jaunes > 0}
-                                        <span class="label label-warning">{$arrayCompo[i].jaunes}</span>
-                                    {/if}
-                                </td>
-                                <td class="text-center">
-                                    {if $arrayCompo[i].rouges > 0}
-                                        <span class="label label-danger">{$arrayCompo[i].rouges}</span>
-                                    {/if}
-                                </td>
-                            </tr>
-                        {sectionelse}
-                            <tr>
-                                <td colspan="6" class="text-center">{#Information_non_disponible#}</td>
-                            </tr>
-                        {/section}
-                    </tbody>
-                </table>
-            {/if}
+                </div> *}
+        <table class='table table-condensed' id='tableStats'>
+          <thead>
+            <tr class='header'>
+              <th class="text-center">#</th>
+              <th>{#Nom#}</th>
+              <th class="text-center">{#Buts#}</th>
+              <th class="bg-success text-center">{#C_V#}</th>
+              <th class="bg-warning text-center">{#C_J#}</th>
+              <th class="bg-danger text-center">{#C_R#}</th>
+            </tr>
+          </thead>
+          <tbody>
+            {section name=i loop=$arrayCompo}
+              <tr class='{cycle values="impair,pair"}'>
+                <td class="text-center">
+                  {if $arrayCompo[i].Capitaine == 'E'}
+                    <span class="label label-default" title="Staff">S</span>
+                  {else}
+                    {$arrayCompo[i].Numero}
+                  {/if}
+                </td>
+                <td>
+                  {$arrayCompo[i].Nom|upper} {$arrayCompo[i].Prenom|upper}
+                  {if $arrayCompo[i].Capitaine == 'C'}
+                    <span class="label label-default" title="{#Capitaine#}">C</span>
+                  {/if}
+                </td>
+                <td class="text-center">
+                  {if $arrayCompo[i].buts > 0}
+                    <span class="badgeg">{$arrayCompo[i].buts}</span>
+                  {/if}
+                </td>
+                <td class="text-center">
+                  {if $arrayCompo[i].verts > 0}
+                    <span class="label label-success">{$arrayCompo[i].verts}</span>
+                  {/if}
+                </td>
+                <td class="text-center">
+                  {if $arrayCompo[i].jaunes > 0}
+                    <span class="label label-warning">{$arrayCompo[i].jaunes}</span>
+                  {/if}
+                </td>
+                <td class="text-center">
+                  {if $arrayCompo[i].rouges > 0}
+                    <span class="label label-danger">{$arrayCompo[i].rouges}</span>
+                  {/if}
+                </td>
+              </tr>
+              {sectionelse}
+              <tr>
+                <td colspan="6" class="text-center">{#Information_non_disponible#}</td>
+              </tr>
+            {/section}
+          </tbody>
+        </table>
+      {/if}
 
-            {if $eTeam}
-                <div class="col-sm-12" id="equipeTeam">
-                    <a href="{$eTeam}?v={$NUM_VERSION}" target="_blank">
-                        <img class="img-responsive img-thumbnail" src="{$eTeam}?v={$NUM_VERSION}" alt="{$nomEquipe}" title="{$nomEquipe}">
-                    </a>
-                    <span class="pull-right badge">{$eSeason2}</span>
-                </div>
-            {/if}
+      {if $eTeam}
+        <div class="col-sm-12" id="equipeTeam">
+          <a href="{$eTeam}?v={$NUM_VERSION}" target="_blank">
+            <img class="img-responsive img-thumbnail" src="{$eTeam}?v={$NUM_VERSION}" alt="{$nomEquipe}"
+              title="{$nomEquipe}">
+          </a>
+          <span class="pull-right badge">{$eSeason2}</span>
+        </div>
+      {/if}
 
-        </article>
-    </div>
+    </article>
+  </div>
 {/if}
