cf272d56ab0d211477f74186014781923972ccc0
laurentgarrigue <lgarrigue@gmail.com>
2022-03-16 13:36:40 +0100

    Feat: group reorder

--- sources/js/GestionGroupe.js
+++ sources/js/GestionGroupe.js
@@ -1,68 +1,75 @@
-jq = jQuery.noConflict();
+jq = jQuery.noConflict()
 
-var langue = [];
+var langue = []
 
-if(lang == 'en')  {
-    langue['Confirmer_MAJ'] = 'Confirm update ?';
-    langue['Nom_evt_vide'] = 'Event name is empty, unable to create';
+if (lang == 'en') {
+    langue['Confirmer_MAJ'] = 'Confirm update ?'
+    langue['Nom_evt_vide'] = 'Event name is empty, unable to create'
 } else {
-    langue['Confirmer_MAJ'] = 'Confirmez-vous le changement ?';
-    langue['Nom_evt_vide'] = 'Le Nom de l\'événement est vide, ajout impossible';
+    langue['Confirmer_MAJ'] = 'Confirmez-vous le changement ?'
+    langue['Nom_evt_vide'] = 'Le Nom de l\'événement est vide, ajout impossible'
 }
 
-function validGroupe()
-{
-//    var libelle = jq('#Libelle').value;
-//    if (libelle.length == 0)
-//    {
-//        alert(langue['Nom_evt_vide']);
-//        return false;
-//    }
+function validGroupe () {
+    //    var libelle = jq('#Libelle').value;
+    //    if (libelle.length == 0)
+    //    {
+    //        alert(langue['Nom_evt_vide']);
+    //        return false;
+    //    }
 
-    return true;
+    return true
 }
 
-function addGroupe()
-{
-	if (!validGroupe())
-		return;
+function addGroupe () {
+    if (!validGroupe())
+        return
 
-	jq('#Cmd').val('Add');
-    jq('#ParamCmd').val();
-    jq('#formGroupe').submit();
-}
-		
-function updateGroupe()
-{
-	if (!validGroupe())
-		return;
-						
-	jq('#Cmd').val('Update');
-    jq('#ParamCmd').val();
-    jq('#formGroupe').submit();
+    jq('#Cmd').val('Add')
+    jq('#ParamCmd').val()
+    jq('#formGroupe').submit()
 }
 
-function razGroupe()
-{
-	jq('#Cmd').val('Raz');
-    jq('#ParamCmd').val();
-    jq('#formGroupe').submit();
+function updateGroupe () {
+    if (!validGroupe())
+        return
+
+    jq('#Cmd').val('Update')
+    jq('#ParamCmd').val()
+    jq('#formGroupe').submit()
+}
+
+function razGroupe () {
+    jq('#Cmd').val('Raz')
+    jq('#ParamCmd').val()
+    jq('#formGroupe').submit()
 }
 
 
-function editGroupe(idGroupe)
-{
-	jq('#Cmd').val('Edit');
-    jq('#ParamCmd').val(idGroupe);
-    jq('#formGroupe').submit();
+function editGroupe (idGroupe) {
+    jq('#Cmd').val('Edit')
+    jq('#ParamCmd').val(idGroupe)
+    jq('#formGroupe').submit()
 }
-	
-function removeGroupe(idGroupe)
-{
-	if(confirm('Supprimer le groupe ?')) {
-        jq('#Cmd').val('Remove');
-        jq('#ParamCmd').val(idGroupe);
-        jq('#formGroupe').submit();
+
+function removeGroupe (idGroupe) {
+    if (confirm('Supprimer le groupe ?')) {
+        jq('#Cmd').val('Remove')
+        jq('#ParamCmd').val(idGroupe)
+        jq('#formGroupe').submit()
     }
 }
-	
+
+jq('.ordre_up').click(function () {
+    jq('#Cmd').val('UpOrder')
+    jq('#idGroupe').val(jq(this).data('id'))
+    jq('#ParamCmd').val(jq(this).data('order'))
+    jq('#formGroupe').submit()
+})
+function UpOrder (idGroupe) {
+    if (confirm('Supprimer le groupe ?')) {
+        jq('#Cmd').val('Remove')
+        jq('#ParamCmd').val(idGroupe)
+        jq('#formGroupe').submit()
+    }
+}
