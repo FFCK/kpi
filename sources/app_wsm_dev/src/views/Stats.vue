1984a34147b223f386bcafc7cb5cde2ae122d146
laurentgarrigue <lgarrigue@gmail.com>
2022-08-03 23:18:03 +0200

    Feat: goal stops

--- sources/app_wsm_dev/src/views/Stats.vue
+++ sources/app_wsm_dev/src/views/Stats.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="container-fluid" v-if="prefs">
-    <h1>KPI Stats</h1>
+    <h1 v-if="!network">KPI Stats</h1>
     <div class="row text-center" v-if="events">
-      <div>
-        {{ $t("Event") }}
+      <div class="col-7">
+        <label v-if="!network">{{ $t("Event") }}</label>
         <select v-model="statsEvent" :disabled="network" class="m-1">
             <option v-for="event in events" :value="event.id" :key="event.id">{{ event.id }} - {{ event.libelle }}</option>
         </select>
-        {{ $t("Pitch") }}
+      </div>
+      <div class="col-3">
+        <label v-if="!network">{{ $t("Pitch") }}</label>
         <select v-model="statsPitch" :disabled="network" class="m-1">
             <option v-for="(n, index) in 8" :key="index" :value="n">{{ $t("Pitch") }} {{ n }}</option>
         </select>
+      </div>
+      <div class="col-2">
         <span v-if="statsEvent > 0 && statsPitch > 0" class="m-1">
           <button v-if="!network" class="btn btn-sm btn-success" @click="connect">Connect</button>
           <button v-if="network" class="btn btn-sm btn-danger" @click="disconnect">Stop</button>
@@ -26,15 +30,7 @@
             <span id="score1" class="badge bg-dark text-light float-end score p-2">{{ score1 }}</span>
           </div>
         </div>
-        <div class="col text-center">
-          <div id="equipe2" class="p-2" :style="'color: ' + game.equipe2.colortext + '; background-color: ' + game.equipe2.color1 +';'">
-            <span id="score2" class="badge bg-dark text-light float-start score p-2">{{ score2 }}</span>
-            <b >{{ game.equipe2.nom }}</b>
-          </div>
-        </div>
-      </div>
-      <div class="row mt-1 text-center">
-        <div class="col">
+        <div class="col-3 text-center">
           <button
             id="match_horloge"
             :class="{
@@ -47,7 +43,15 @@
           >{{ matchHorloge }}</button>
           <span id="match_periode" class="badge bg-dark text-light">{{ matchPeriodFormated }}</span>
         </div>
+        <div class="col text-center">
+          <div id="equipe2" class="p-2" :style="'color: ' + game.equipe2.colortext + '; background-color: ' + game.equipe2.color1 +';'">
+            <span id="score2" class="badge bg-dark text-light float-start score p-2">{{ score2 }}</span>
+            <b >{{ game.equipe2.nom }}</b>
+          </div>
+        </div>
       </div>
+      <!-- <div class="row mt-1 text-center">
+      </div> -->
       <div class="row mt-1 gx-1">
         <div class="col border text-center p-1" :style="'color: ' + game.equipe1.colortext + '; background-color: ' + game.equipe1.color1 +';'">
           <div class="row g-1">
@@ -55,37 +59,35 @@
               <button
                 :class="{
                   btn: true,
-                  'btn-lg': true,
+                  'btn-sm': true,
                   'text-light': true,
                   'btn-secondary': [0, 3, 6, 7].includes(btnMode),
                   'btn-success': btnMode === 1,
                   'btn-danger': btnMode === 2,
                   'btn-primary': btnMode === 4,
-                  'btn-info': btnMode === 5,
+                  'btn-warning': btnMode === 5,
                   }"
                 :disabled="[3, 6, 7].includes(btnMode)"
                 v-if="player.matric"
                 @click="action(1, player.matric)">
                 <span>{{ player.Numero }}</span>
               </button>
-              <button class="btn btn-lg" v-else>
+              <button class="btn btn-sm" v-else>
                 &nbsp;
               </button>
             </span>
-          </div>
-          <div class="row">
-            <span class="col-12">
+            <span class="col-md-12 col-sm-8">
               <button
                 :class="{
                   btn: true,
-                  'btn-lg': true,
+                  'btn-sm': true,
                   'text-light': true,
                   'm-1': true,
                   'btn-secondary': [0, 3, 6, 7].includes(btnMode),
                   'btn-success': btnMode === 1,
                   'btn-danger': btnMode === 2,
                   'btn-primary': btnMode === 4,
-                  'btn-info': btnMode === 5,
+                  'btn-warning': btnMode === 5,
                   }"
                 :disabled="[3, 6, 7].includes(btnMode)"
                 @click="action(1, 0)">
@@ -96,41 +98,39 @@
         </div>
         <div class="col border text-center p-1" :style="'color: ' + game.equipe2.colortext + '; background-color: ' + game.equipe2.color1 +';'">
           <div class="row g-1">
-            <span class="col-md-2 col-sm-4 col-xs-6" v-for="(player, index) in team2" :key="index">
+            <span class="col-md-2 col-sm-4" v-for="(player, index) in team2" :key="index">
               <button
                 :class="{
                   btn: true,
-                  'btn-lg': true,
+                  'btn-sm': true,
                   'text-light': true,
                   'btn-secondary': [0, 2, 4, 5].includes(btnMode),
                   'btn-success': btnMode === 1,
                   'btn-danger': btnMode === 3,
                   'btn-primary': btnMode === 6,
-                  'btn-info': btnMode === 7,
+                  'btn-warning': btnMode === 7,
                   }"
                 :disabled="[2, 4, 5].includes(btnMode)"
                 v-if="player.matric"
                 @click="action(2, player.matric)">
                 <span>{{ player.Numero }}</span>
               </button>
-              <button class="btn btn-lg" v-else>
+              <button class="btn btn-sm" v-else>
                 &nbsp;
               </button>
             </span>
-          </div>
-          <div class="row">
-            <span class="col-12">
+            <span class="col-md-12 col-sm-8">
               <button
                 :class="{
                   btn: true,
-                  'btn-lg': true,
+                  'btn-sm': true,
                   'text-light': true,
                   'm-1': true,
                   'btn-secondary': [0, 2, 4, 5].includes(btnMode),
                   'btn-success': btnMode === 1,
                   'btn-danger': btnMode === 3,
                   'btn-primary': btnMode === 6,
-                  'btn-info': btnMode === 7,
+                  'btn-warning': btnMode === 7,
                   }"
                 :disabled="[2, 4, 5].includes(btnMode)"
                 @click="action(2, 0)">
@@ -147,11 +147,11 @@
             :title="$t('Kickoff_won')"
             :class="{
               btn: true,
-              'btn-lg': true,
+              'btn-sm': true,
               'btn-outline-success': btnMode !== 1,
               'btn-success': btnMode === 1
             }"
-            :disabled="btnMode !== 1"
+            :disabled="btnMode >= 4 && btnMode <= 7"
             @click="btnMode = 1">
             {{ $t("Kickoff") }} <i class="bi bi-emoji-sunglasses"></i>
           </button>
@@ -161,7 +161,7 @@
             :title="$t('Kickoff_lost')"
             :class="{
               btn: true,
-              'btn-lg': true,
+              'btn-sm': true,
               'btn-outline-danger': btnMode !== 2,
               'btn-danger': btnMode === 2
             }"
@@ -175,7 +175,7 @@
             :title="$t('Kickoff_lost')"
             :class="{
               btn: true,
-              'btn-lg': true,
+              'btn-sm': true,
               'btn-outline-danger': btnMode !== 3,
               'btn-danger': btnMode === 3
             }"
@@ -189,11 +189,11 @@
             :title="$t('Kickoff_won')"
             :class="{
               btn: true,
-              'btn-lg': true,
+              'btn-sm': true,
               'btn-outline-success': btnMode !== 1,
               'btn-success': btnMode === 1
             }"
-            :disabled="btnMode !== 1"
+            :disabled="btnMode >= 4 && btnMode <= 7"
             @click="btnMode = 1">
             {{ $t("Kickoff") }} <i class="bi bi-emoji-sunglasses"></i>
           </button>
@@ -204,41 +204,41 @@
             :title="$t('Shot_on')"
             :class="{
               btn: true,
-              'btn-lg': true,
+              'btn-sm': true,
               'btn-outline-primary': btnMode !== 4,
               'btn-primary': btnMode === 4
             }"
             :disabled="btnMode > 0 && btnMode < 4"
             @click="btnMode = 4">
-            {{ $t("Shot") }} <i class="bi bi-box-arrow-in-up"></i>
+            {{ $t("Shot") }} <i class="bi bi-caret-up-square-fill"></i>
           </button>
         </div>
         <div class="d-grid col-3">
           <button
-            :title="$t('Shot_off')"
+            :title="$t('Shot_stop')"
             :class="{
               btn: true,
-              'btn-lg': true,
-              'btn-outline-info': btnMode !== 5,
-              'btn-info': btnMode === 5
+              'btn-sm': true,
+              'btn-outline-warning': btnMode !== 5,
+              'btn-warning': btnMode === 5
             }"
             :disabled="btnMode > 0 && btnMode < 4"
             @click="btnMode = 5">
-            {{ $t("Shot") }} <i class="bi bi-box-arrow-up-right"></i>
+            {{ $t("Stop") }} <i class="bi bi-x-square-fill"></i>
           </button>
         </div>
         <div class="d-grid col-3">
           <button
-            :title="$t('Shot_off')"
+            :title="$t('Shot_stop')"
             :class="{
               btn: true,
-              'btn-lg': true,
-              'btn-outline-info': btnMode !== 7,
-              'btn-info': btnMode === 7
+              'btn-sm': true,
+              'btn-outline-warning': btnMode !== 7,
+              'btn-warning': btnMode === 7
             }"
             :disabled="btnMode > 0 && btnMode < 4"
             @click="btnMode = 7">
-            {{ $t("Shot") }} <i class="bi bi-box-arrow-up-right"></i>
+            {{ $t("Stop") }} <i class="bi bi-x-square-fill"></i>
           </button>
         </div>
         <div class="d-grid col-3">
@@ -246,13 +246,13 @@
             :title="$t('Shot_on')"
             :class="{
               btn: true,
-              'btn-lg': true,
+              'btn-sm': true,
               'btn-outline-primary': btnMode !== 6,
               'btn-primary': btnMode === 6
             }"
             :disabled="btnMode > 0 && btnMode < 4"
             @click="btnMode = 6">
-            {{ $t("Shot") }} <i class="bi bi-box-arrow-in-up"></i>
+            {{ $t("Shot") }} <i class="bi bi-caret-up-square-fill"></i>
           </button>
         </div>
 
@@ -261,7 +261,7 @@
             :title="$t('Pass_possession')"
             :class="{
               btn: true,
-              'btn-lg': true,
+              'btn-sm': true,
               'btn-outline-secondary': btnMode > 0,
               'btn-secondary': btnMode === 0
             }"
@@ -273,7 +273,7 @@
         <div class="d-grid col-2">
           <button
             :title="$t('Neutral_ball')"
-            class="btn btn-lg btn-dark"
+            class="btn btn-sm btn-dark"
             @click="btnMode = 0">
             {{ $t("Neutral") }}
           </button>
@@ -283,7 +283,7 @@
             :title="$t('Pass_possession')"
             :class="{
               btn: true,
-              'btn-lg': true,
+              'btn-sm': true,
               'btn-outline-secondary': btnMode > 0,
               'btn-secondary': btnMode === 0
             }"
@@ -347,6 +347,7 @@ export default {
         this.socketStats = null
       }
       this.network = null
+      document.querySelector('.top-margin').style.display = 'block'
     },
     fetchGameRotate () {
       if (!this.intervalGame) {
@@ -374,7 +375,7 @@ export default {
             }
           })
       }
-      if (this.gameTarget !== this.game?.id_match) {
+      if (this.gameTarget && this.gameTarget !== this.game?.id_match) {
         try {
           const resultGameFetch = await liveApi.getGame(this.gameTarget)
           const scoreGameFetch = await liveApi.getScore(this.gameTarget)
@@ -404,7 +405,8 @@ export default {
       }
       this.score1 = score.score1
       this.score2 = score.score2
-      this.matchPeriodFormated = score.periode.substr(1)
+      this.matchPeriod = score.periode
+      this.matchPeriodFormated = (score.periode.substring(0, 1) !== 'M') ? 'OVT' + score.periode.substring(1) : score.periode.substring(1)
     },
     async fetchNetwork () {
       if (this.statsEvent > 0) {
@@ -413,6 +415,7 @@ export default {
           .then(resultEventNetwork => {
             if (resultEventNetwork.data) {
               this.network = resultEventNetwork.data.network
+              document.querySelector('.top-margin').style.display = 'none'
               this.wsConnect()
             } else {
               console.log('No network')
@@ -503,30 +506,28 @@ export default {
           break
         case 5:
         case 7:
-          this.submit(this.game['equipe' + team].id, player, 'shot-out')
+          this.submit(this.game['equipe' + team].id, player, 'shot-stop')
           this.btnMode = 0
           break
       }
     },
     submit (team, player, action) {
-      if (this.matchHorlogeStarted) {
-        const obj = {
-          user: this.user,
-          game: this.game.id_match,
-          team: team,
-          player: player,
-          action: action,
-          period: this.matchPeriod,
-          timer: this.matchHorloge.substr(0, 5)
-        }
-        liveApi
-          .setStat(obj)
-          .then(result => {
-            if (result.data) {
-              console.log(result.data)
-            }
-          })
+      const obj = {
+        user: this.user,
+        game: this.game.id_match,
+        team: team,
+        player: player,
+        action: action,
+        period: this.matchPeriod,
+        timer: this.matchHorloge.substr(0, 5)
       }
+      liveApi
+        .setStat(obj)
+        .then(result => {
+          if (result.data) {
+            console.log(result.data)
+          }
+        })
     }
   },
   mounted () {
