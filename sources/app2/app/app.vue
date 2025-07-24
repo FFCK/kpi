db9e32711d802042278de16074c987d36c8ea644
laurentgarrigue <lgarrigue@gmail.com>
2025-07-24 23:16:24 +0200

    Feat: Tailwind + Navbar

--- sources/app2/app/app.vue
+++ sources/app2/app/app.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
+
+<style>
+@import url("~/assets/css/app.css");
+@import "tailwindcss";
+
+</style>
--- sources/app2/nuxt.config.ts
+++ sources/app2/nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite"
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: ['@nuxt/eslint'],
+  css: ['~/assets/css/app.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
