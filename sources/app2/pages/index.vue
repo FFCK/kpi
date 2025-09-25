a22a91f5b69eaf87a6cd9f9eb33c27dc4e496ad1
laurentgarrigue <lgarrigue@gmail.com>
2025-09-25 17:56:44 +0200

    GFeat: EventSelector translation

--- sources/app2/pages/index.vue
+++ sources/app2/pages/index.vue
@@ -9,28 +9,14 @@
       <NuxtLink to="/charts" class="border border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white font-semibold rounded px-4 py-2 text-lg w-2/5 transition-colors text-center">{{ t('nav.Chart') }}</NuxtLink>
     </div>
 
-    <AppAlert>
-      This is an auto-imported component.
-      <br>
-
-      
-
-
-    </AppAlert>
-
-    {{ locale }}
-    <h1>{{ t('message') }}</h1>
-    <p>{{ t('status.Offline') }}</p>
-
     <div class="text-center mt-8">
       <img class="w-30 mx-auto" src="/img/logo_kp.png" alt="kayak-polo" />
-      <p class="text-xs text-gray-500 mt-2">v0.1.0</p>
+      <p class="text-sm text-gray-500 mt-1">v{{ pkg.version }}</p>
     </div>
   </div>
 </template>
 
 <script setup>
-const { t, locale } = useI18n()
-
-watch(locale, (l) => console.log(`Current locale: ${l}`))
+import pkg from '../package.json'
+const { t } = useI18n()
 </script>
