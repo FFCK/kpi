249e8b3822f6bd3ff0a78a6e8e04f6f94d0085a6
laurentgarrigue <lgarrigue@gmail.com>
2025-11-15 12:32:56 +0100

    CFix: api2 swagger access

--- sources/api2/config/bundles.php
+++ sources/api2/config/bundles.php
@@ -8,4 +8,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
