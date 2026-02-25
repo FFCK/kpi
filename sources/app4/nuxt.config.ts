b24953f3ef8ad6692e44c19337d5fab19a3d6664
laurentgarrigue <lgarrigue@gmail.com>
2026-02-25 15:52:26 +0100

    Fix: font loading

--- sources/app4/nuxt.config.ts
+++ sources/app4/nuxt.config.ts
@@ -16,7 +16,10 @@ export default defineNuxtConfig({
         { name: 'description', content: 'KPI Administration Panel' }
       ],
       link: [
-        { rel: 'icon', type: 'image/png', href: `${baseUrl}/favicon.png` }
+        { rel: 'icon', type: 'image/png', href: `${baseUrl}/favicon.png` },
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap' }
       ]
     }
   },
