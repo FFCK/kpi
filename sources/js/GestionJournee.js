f6f73224fa09f0fc7776ac608e8569d84b1c1321
laurentgarrigue <lgarrigue@gmail.com>
2026-03-07 20:47:43 +0100

    Fix: Forfeit

--- sources/js/GestionJournee.js
+++ sources/js/GestionJournee.js
@@ -880,7 +880,7 @@ jq(document).ready(function () { //Jquery + NoConflict='J'
 			jq(this).before('<input type="tel" id="inputZone" class="directInputSpan" tabindex="' + tabindexVal + '" size="2" maxlength="2" value="' + valeur + '">')
 		} else if (jq(this).hasClass('score')) {
 			jq(this).before('<input type="text" inputmode="numeric" id="inputZone" class="directInputSpan" tabindex="' + tabindexVal + '" size="2" maxlength="2" pattern="[0-9Ff]{1,2}" value="' + valeur + '">')
-			jq('#inputZone').on('input', function () {
+			jq('#inputZone').bind('input', function () {
 				this.value = this.value.toUpperCase()
 			})
 		} else if (jq(this).hasClass('equipe')) {
