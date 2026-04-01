1ef78b6572f60f28f6ceca5b0b2a6662421c6b64
laurentgarrigue <lgarrigue@gmail.com>
2026-04-01 23:14:04 +0200

    Feat: home page tutorials

--- sources/app2/pages/index.vue
+++ sources/app2/pages/index.vue
@@ -29,6 +29,53 @@
 
       <EventQrCode />
 
+      <!-- Tutorial screenshots -->
+      <div v-if="hasEventSelected" class="mt-10 pt-8 border-t border-gray-200">
+        <!-- Titre (4) -->
+        <p class="text-center text-lg font-medium text-gray-700 mb-4">
+          <UIcon name="i-heroicons-information-circle" class="h-6 w-6 inline-block mr-1" />
+          {{ t('Event.TutoTitle') }}
+        </p>
+
+        <!-- Desktop: tuto_md.png avec descriptions gauche/droite -->
+        <div class="hidden md:grid md:grid-cols-[1fr_3fr_1fr] items-center gap-6">
+          <p class="text-right text-md text-gray-700">
+            <UIcon name="i-heroicons-users" class="h-5 w-5 inline-block mb-0.5 mr-1" />
+            {{ t('Event.TutoTeam') }}
+          </p>
+          <!-- Image avec opacité (2) + badge overlay (1) -->
+          <div class="relative shadow-xl/30">
+            <img src="/img/tuto_md.png" alt="Tutorial" class="w-full rounded opacity-70" />
+            <span class="absolute top-2 right-2 bg-gray-800/70 text-white text-xs font-medium px-2 py-0.5 rounded italic">
+              {{ t('Event.TutoExample') }}
+            </span>
+          </div>
+          <p class="text-left text-md text-gray-700">
+            <UIcon name="i-heroicons-document-text" class="h-5 w-5 inline-block mb-0.5 mr-1" />
+            {{ t('Event.TutoMatchSheet') }}
+          </p>
+        </div>
+
+        <!-- Mobile: tuto_xs.png avec descriptions au-dessus/dessous -->
+        <div class="md:hidden flex flex-col items-center gap-3">
+          <p class="text-center text-sm text-gray-600 px-4">
+            <UIcon name="i-heroicons-users" class="h-4 w-4 inline-block mb-0.5 mr-1" />
+            {{ t('Event.TutoTeam') }}
+          </p>
+          <!-- Image avec opacité (2) + badge overlay (1) -->
+          <div class="relative w-full max-w-xs">
+            <img src="/img/tuto_xs.png" alt="Tutorial" class="w-full rounded shadow-sm opacity-60" />
+            <span class="absolute top-2 right-2 bg-gray-800/70 text-white text-xs font-medium px-2 py-0.5 rounded italic">
+              {{ t('Event.TutoExample') }}
+            </span>
+          </div>
+          <p class="text-center text-sm text-gray-600 px-4">
+            <UIcon name="i-heroicons-document-text" class="h-4 w-4 inline-block mb-0.5 mr-1" />
+            {{ t('Event.TutoMatchSheet') }}
+          </p>
+        </div>
+      </div>
+
       <div class="text-center mt-8">
         <img class="w-30 mx-auto" src="/img/logo_kp.png" alt="kayak-polo" />
       </div>
